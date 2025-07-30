cb61ca86a61576069f574452be9b88b9043de994
Iulian Donici <iuliandonici@gmail.com>
2025-07-30 16:57:25 +0200

    Updated.

--- functions/f_add_repo_vscodium.sh
+++ functions/f_add_repo_vscodium.sh
@@ -1,27 +1,66 @@
 #!/bin/bash
 function f_add_repo_vscodium() {
     echo "- Currently adding the VS Codium IDE repo using $(f_get_distro_packager):"
-    if [[ "$(f_get_distro_packager)" == "dnf" || "$(f_get_distro_packager)" == "zypper" ]]; then
-        if [[ "$EUID" -ne 0 ]]; then 
-            sudo wget https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-            -O /usr/share/keyrings/vscodium-archive-keyring.asc
-# Setting a variable for getting the machine's architecture
+    if [[ "$(f_get_distro_packager)" == "dnf" ]]; then
+        if [[ "$EUID" -ne 0 ]]; then # Setting a variable for getting the machine's architecture
             architecture=$(uname -m)
             if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
-                echo 'deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.asc ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
-                | sudo tee /etc/apt/sources.list.d/vscodium.list
+            tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
+                [gitlab.com_paulcarroty_vscodium_repo]
+                name=gitlab.com_paulcarroty_vscodium_repo
+                baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+                enabled=1
+                gpgcheck=1
+                repo_gpgcheck=1
+                gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+                metadata_expire=1h
+EOF
             else
                 echo "There is no version of VSCodium for x86."
             fi            
         else
-            wget https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-            -O /usr/share/keyrings/vscodium-archive-keyring.asc
 # Setting a variable for getting the machine's architecture
             architecture=$(uname -m)
             if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
                 echo 'deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.asc ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
                 | tee /etc/apt/sources.list.d/vscodium.list
            else
+                echo "There is no VSCodium for this architecture."
+           fi
+        fi
+    if [[ "$(f_get_distro_packager)" == "zypper" ]]; then
+        if [[ "$EUID" -ne 0 ]]; then # Setting a variable for getting the machine's architecture
+            architecture=$(uname -m)
+            if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
+            sudo tee -a /etc/zypp/repos.d/vscodium.repo << 'EOF'
+                [gitlab.com_paulcarroty_vscodium_repo]
+                name=gitlab.com_paulcarroty_vscodium_repo
+                baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+                enabled=1
+                gpgcheck=1
+                repo_gpgcheck=1
+                gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+                metadata_expire=1h
+EOF
+
+            else
+                echo "There is no version of VSCodium for x86."
+            fi            
+        else
+# Setting a variable for getting the machine's architecture
+            architecture=$(uname -m)
+            if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
+            tee -a /etc/zypp/repos.d/vscodium.repo << 'EOF'
+                [gitlab.com_paulcarroty_vscodium_repo]
+                name=gitlab.com_paulcarroty_vscodium_repo
+                baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+                enabled=1
+                gpgcheck=1
+                repo_gpgcheck=1
+                gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+                metadata_expire=1h
+EOF           
+            else
                 echo "There is no VSCodium for this architecture."
             fi
         fi
