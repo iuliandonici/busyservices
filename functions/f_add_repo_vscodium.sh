dd91fedf989594acaa902cf7281ac42c6096dff9
Iulian Donici <iuliandonici@gmail.com>
2025-07-30 14:14:32 +0200

    Updated.

--- functions/f_add_repo_vscodium.sh
+++ functions/f_add_repo_vscodium.sh
@@ -41,7 +41,7 @@ function f_add_repo_vscodium() {
             architecture=$(uname -m)
             if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
                 echo 'deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.asc ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
-                | sudo tee /etc/apt/sources.list.d/vscodium.list
+                | tee /etc/apt/sources.list.d/vscodium.list
            else
                 echo "There is no VSCodium for this architecture."
             fi
