873622f9705b2447dc4f01bfe1abfa60191e5ad3
Iulian Donici <iuliandonici@gmail.com>
2025-07-30 17:00:31 +0200

    Updated.

--- functions/f_add_repo_vscodium.sh
+++ functions/f_add_repo_vscodium.sh
@@ -6,14 +6,14 @@ function f_add_repo_vscodium() {
             architecture=$(uname -m)
             if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
             tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
-                [gitlab.com_paulcarroty_vscodium_repo]
-                name=gitlab.com_paulcarroty_vscodium_repo
-                baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
-                enabled=1
-                gpgcheck=1
-                repo_gpgcheck=1
-                gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
-                metadata_expire=1h
+[gitlab.com_paulcarroty_vscodium_repo]
+name=gitlab.com_paulcarroty_vscodium_repo
+baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+metadata_expire=1h
 EOF
             else
                 echo "There is no version of VSCodium for x86."
