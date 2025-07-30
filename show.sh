8e51ccd7ca5587e6daa5813dcb3a3b28dcb4a368
Iulian Donici <iuliandonici@gmail.com>
2025-07-30 17:28:32 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -34,7 +34,8 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
 metadata_expire=1h
-EOF           else
+EOF           
+            else
                 echo "There is no VSCodium for this architecture."
            fi
         fi
@@ -42,7 +43,7 @@ EOF           else
         if [[ "$EUID" -ne 0 ]]; then # Setting a variable for getting the machine's architecture
             architecture=$(uname -m)
             if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
-            sudo tee -a /etc/zypp/repos.d/vscodium.repo << 'EOF'
+                sudo tee -a /etc/zypp/repos.d/vscodium.repo << 'EOF'
                 [gitlab.com_paulcarroty_vscodium_repo]
                 name=gitlab.com_paulcarroty_vscodium_repo
                 baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
@@ -60,15 +61,15 @@ EOF
 # Setting a variable for getting the machine's architecture
             architecture=$(uname -m)
             if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
-                tee -a /etc/zypp/repos.d/vscodium.repo << `EOF`
-                    [gitlab.com_paulcarroty_vscodium_repo]
-                    name=gitlab.com_paulcarroty_vscodium_repo
-                    baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
-                    enabled=1
-                    gpgcheck=1
-                    repo_gpgcheck=1
-                    gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
-                    metadata_expire=1h
+            tee -a /etc/zypp/repos.d/vscodium.repo << "EOF"
+[gitlab.com_paulcarroty_vscodium_repo]
+name=gitlab.com_paulcarroty_vscodium_repo
+baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+metadata_expire=1h
 EOF           
             else
                 echo "There is no VSCodium for this architecture."
