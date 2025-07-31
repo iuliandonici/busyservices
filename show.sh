84979d78d3a83f1123135878c47bbc8f39626230
Iulian Donici <iuliandonici@gmail.com>
2025-07-31 20:32:37 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -61,30 +61,32 @@ metadata_expire=1h" >> /etc/yum.repos.d/vscodium.repo
                 echo "There is no VSCodium for this architecture."
             fi
         fi
-    elif [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
-        if [[ "$EUID" -ne 0 ]]; then 
-            sudo wget https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-            -O /usr/share/keyrings/vscodium-archive-keyring.asc
-# Setting a variable for getting the machine's architecture
-            architecture=$(uname -m)
-            if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
-                echo 'deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.asc ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
-                | sudo tee /etc/apt/sources.list.d/vscodium.list
-            else
-                echo "There is no version of VSCodium for x86."
-            fi            
-        else
-            wget https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-            -O /usr/share/keyrings/vscodium-archive-keyring.asc
-# Setting a variable for getting the machine's architecture
-            architecture=$(uname -m)
-            if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
-                echo 'deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.asc ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
-                | tee /etc/apt/sources.list.d/vscodium.list
-           else
-                echo "There is no VSCodium for this architecture."
-            fi
-        fi
     fi
+}
+#     elif [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
+#         if [[ "$EUID" -ne 0 ]]; then 
+#             sudo wget https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+#             -O /usr/share/keyrings/vscodium-archive-keyring.asc
+# # Setting a variable for getting the machine's architecture
+#             architecture=$(uname -m)
+#             if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
+#                 echo 'deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.asc ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
+#                 | sudo tee /etc/apt/sources.list.d/vscodium.list
+#             else
+#                 echo "There is no version of VSCodium for x86."
+#             fi            
+#         else
+#             wget https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+#             -O /usr/share/keyrings/vscodium-archive-keyring.asc
+# # Setting a variable for getting the machine's architecture
+#             architecture=$(uname -m)
+#             if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
+#                 echo 'deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.asc ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
+#                 | tee /etc/apt/sources.list.d/vscodium.list
+#            else
+#                 echo "There is no VSCodium for this architecture."
+#             fi
+#         fi
+#     fi
 }
 f_add_repo_vscodium
