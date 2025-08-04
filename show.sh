1836e5bab69e2ce0ccd96160128eda72876580d3
Iulian Donici <iuliandonici@gmail.com>
2025-08-04 17:52:47 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -95,7 +95,7 @@ function f_add_repo_docker() {
                 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
                 $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
                 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                            else
+            else
                 echo "There is no version of Docker for x86."
             fi            
         else
