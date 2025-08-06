0d06cc9efa455234b6117ad064d13195e998eaf4
Iulian Donici <iuliandonici@gmail.com>
2025-08-06 11:19:30 +0200

    Updated.

--- functions/f_install_extra_software.sh
+++ functions/f_install_extra_software.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-var_install_extra_software_array=("docker-ce" "docker-ce-cli" "containerd.io" "docker-compose-plugin")
+var_install_server_prod_software_array=("docker-ce" "docker-ce-cli" "containerd.io" "docker-compose-plugin")
 function f_install_extra_software() {
     source functions/f_update_software.sh
     source functions/f_add_repo_docker.sh
@@ -8,38 +8,38 @@ function f_install_extra_software() {
     f_add_repo_docker
     f_update_software
     echo "- List of extra software that will be installed using $(f_get_distro_packager):"
-    for i in "${!var_install_extra_software_array[@]}"
+    for i in "${!var_install_server_prod_software_array[@]}"
     do
-        echo " $i ${var_install_extra_software_array[$i]}"
+        echo " $i ${var_install_server_prod_software_array[$i]}"
     done
     if [[ $(f_get_distro_packager) == "apk" ]]; then
-        for i in "${!var_install_extra_software_array[@]}"
+        for i in "${!var_install_server_prod_software_array[@]}"
         do
-            echo "- Currently installing: $i ${var_install_extra_software_array[$i]}"
+            echo "- Currently installing: $i ${var_install_server_prod_software_array[$i]}"
             if [[ "$EUID" -ne 0 ]]; then 
-                sudo $(f_get_distro_packager) add ${var_install_extra_software_array[$i]}  
+                sudo $(f_get_distro_packager) add ${var_install_server_prod_software_array[$i]}  
             else
-                $(f_get_distro_packager) add ${var_install_extra_software_array[$i]}  
+                $(f_get_distro_packager) add ${var_install_server_prod_software_array[$i]}  
             fi
         done     
     elif [[ $(f_get_distro_packager) == "dnf" || $(f_get_distro_packager) == "zypper" ]]; then
-        for i in "${!var_install_extra_software_array[@]}"
+        for i in "${!var_install_server_prod_software_array[@]}"
         do
-            echo "- Currently installing: $i ${var_install_extra_software_array[$i]}"
+            echo "- Currently installing: $i ${var_install_server_prod_software_array[$i]}"
             if [[ "$EUID" -ne 0 ]]; then 
-                sudo $(f_get_distro_packager) install -y ${var_install_extra_software_array[$i]}  
+                sudo $(f_get_distro_packager) install -y ${var_install_server_prod_software_array[$i]}  
             else
-                $(f_get_distro_packager) install -y ${var_install_extra_software_array[$i]}  
+                $(f_get_distro_packager) install -y ${var_install_server_prod_software_array[$i]}  
             fi
         done        
     else
-        for i in "${!var_install_extra_software_array[@]}"
+        for i in "${!var_install_server_prod_software_array[@]}"
         do
-            echo "- Currently installing: $i ${var_install_extra_software_array[$i]}"
+            echo "- Currently installing: $i ${var_install_server_prod_software_array[$i]}"
             if [[ "$EUID" -ne 0 ]]; then 
-                sudo $(f_get_distro_packager) install -y ${var_install_extra_software_array[$i]}  
+                sudo $(f_get_distro_packager) install -y ${var_install_server_prod_software_array[$i]}  
             else
-                $(f_get_distro_packager) install -y ${var_install_extra_software_array[$i]}  
+                $(f_get_distro_packager) install -y ${var_install_server_prod_software_array[$i]}  
             fi
         done
     fi
--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@
 source functions/f_get_distro_id.sh
 source functions/f_install_base_software.sh
 source functions/f_install_extra_software.sh
-source functions/f_config_docker.sh
 case $(f_get_distro_id) in
     ubuntu | debian | linuxmint | alpine | almalinux | fedora | opensuse-leap)
         echo "Initializing updates and installation of packages for $(f_get_distro_id)."
