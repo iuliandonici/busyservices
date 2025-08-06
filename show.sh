ac291db06c154f28d76ce1a4c227a4e2f7f3f344
Iulian Donici <iuliandonici@gmail.com>
2025-08-06 11:45:24 +0200

    Updated.

--- show.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source functions/f_config_docker.sh
-source functions/f_add_repo_docker.sh
-# source functions/f_install_base_software.sh
-# source functions/f_install_extra_software.sh
-f_add_repo_docker
-f_config_docker
