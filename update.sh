57e3e339a8005b84bb5b476bb0644cd6d51bd4d2
Iulian Donici <iuliandonici@gmail.com>
2025-08-06 11:40:23 +0200

    Updated.

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source functions/f_get_distro_id.sh
 source functions/f_install_base_software.sh
-source functions/f_install_server_dev_software.sh
 case $(f_get_distro_id) in
     ubuntu | debian | linuxmint | alpine | almalinux | fedora | opensuse-leap)
         echo "Initializing updates and installation of packages for $(f_get_distro_id)."
@@ -12,10 +11,11 @@ esac
 case $(hostname) in
     busyt | mycontainer)
         echo "This is $(hostname)"
-        # f_install_extra_software
+        f_install_desktop_dev_software
     ;;
     busydev)
         echo "This is $(hostname)"
+        source functions/f_install_server_dev_software.sh
         f_install_server_dev_software
         # ./docker/install_server_docker.sh
     ;;
