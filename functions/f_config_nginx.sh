85d161b1acf447df18e571ee7f99372a7db906f1
Iulian Donici <iuliandonici@gmail.com>
2025-08-12 17:03:47 +0200

    Updated.

--- functions/f_config_nginx.sh
+++ functions/f_config_nginx.sh
@@ -4,17 +4,26 @@ function f_config_nginx() {
     if [[ -f /usr/sbin/nginx ]]; then
         echo "- Nginx is installed, now we'll config it."
         if [[ "$EUID" -ne 0 ]]; then 
-            sudo mkdir /var/www/prod/
+            sudo mkdir /var/www/busyprod/
             sudo rm -rf /var/www/html/
+            sudo rm -rf /etc/nginx/sites-enabled/default
+            sudo rm -rf /etc/nginx/sites-available/default
             sudo cp -r functions/f_config_nginx.html index.html
-            sudo mv index.html /var/www/prod/
+            sudo mv index.html /var/www/busyprod/
+            sudo cp -r functions/f_config_nginx busyprod
+            sudo mv busyprod /etc/nginx/sites-available/
+            sudo ln -s /etc/nginx/sites-available/busyprod /etc/nginx/sites-enabled/busyprod
             sudo service nginx restart
         else
-
-            mkdir /var/www/prod/
+            sudo mkdir /var/www/busyprod/
             rm -rf /var/www/html/
+            rm -rf /etc/nginx/sites-enabled/default
+            rm -rf /etc/nginx/sites-available/default
             cp -r functions/f_config_nginx.html index.html
-            mv index.html /var/www/prod/
+            mv index.html /var/www/busyprod/
+            cp -r functions/f_config_nginx busyprod
+            mv busyprod /etc/nginx/sites-available/
+            ln -s /etc/nginx/sites-available/busyprod /etc/nginx/sites-enabled/busyprod
             service nginx restart
         fi
     else
