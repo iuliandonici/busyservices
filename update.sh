6f792035671e093671a8bd3efb5d3b3f9088f0ff
Iulian Donici <iuliandonici@gmail.com>
2025-08-12 16:21:09 +0200

    Updated.

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 source functions/f_get_distro_id.sh
 source functions/f_install_base_software.sh
 source functions/f_install_server_prod_software.sh
-source functions/f_install_nginx_requirements.sh
+source functions/f_install_nginx_requirements.sha
 case $(f_get_distro_id) in
     ubuntu | debian | linuxmint | alpine | almalinux | fedora | opensuse-leap)
         echo "Initializing updates and installation of packages for $(f_get_distro_id)."
