69833f02de2f0adfc74325b7b729e43dadfa8d0c
Iulian Donici <iuliandonici@gmail.com>
2025-08-12 13:52:11 +0200

    Testing Transmission in production.

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source functions/f_get_distro_id.sh
 source functions/f_install_base_software.sh
+source functions/f_install_transmission_requirements.sh
+source functions/f_config_transmission.sh
 case $(f_get_distro_id) in
     ubuntu | debian | linuxmint | alpine | almalinux | fedora | opensuse-leap)
         echo "Initializing updates and installation of packages for $(f_get_distro_id)."
@@ -9,14 +11,19 @@ case $(f_get_distro_id) in
 esac
 case $(hostname) in
     busyt | mycontainer)
-        echo "This is $(hostname)"
+        echo "This is $(hostname)."
         source functions/f_install_desktop_dev_software.sh
         f_install_desktop_dev_software
     ;;
     busydev)
-        echo "This is $(hostname)"
+        echo "This is $(hostname)."
         source functions/f_install_server_dev_software.sh
         f_install_server_dev_software
         # ./docker/install_server_docker.sh
+    ;;
+    busycenter)
+        echo "This is $(hostname)."
+        f_install_transmission_requirements
+        f_config_transmission
     ;;
 esac
