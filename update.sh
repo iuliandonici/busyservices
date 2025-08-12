f8c5f34a1ddc8a79695cc10792634d60ce0677cf
Iulian Donici <iuliandonici@gmail.com>
2025-08-12 16:24:51 +0200

    Updated.

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 source functions/f_get_distro_id.sh
 source functions/f_install_base_software.sh
-source functions/f_install_server_prod_software.sh
-source functions/f_install_nginx_requirements.sha
 case $(f_get_distro_id) in
     ubuntu | debian | linuxmint | alpine | almalinux | fedora | opensuse-leap)
         echo "Initializing updates and installation of packages for $(f_get_distro_id)."
@@ -18,6 +16,8 @@ case $(hostname) in
     busydev)
         echo "This is $(hostname)."
         source functions/f_install_server_dev_software.sh
+        # source functions/f_install_server_prod_software.sh
+        source functions/f_install_nginx_requirements.sha
         f_install_server_dev_software
         f_install_nginx_requirements
     ;;
