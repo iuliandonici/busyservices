c6cedec8b368fafa23b49ac9807fc344f3d20f7f
Iulian Donici <iuliandonici@gmail.com>
2025-08-12 16:12:02 +0200

    Updated.

--- functions/f_config_nginx.sh
+++ functions/f_config_nginx.sh
@@ -6,14 +6,12 @@ function f_config_nginx() {
         if [[ "$EUID" -ne 0 ]]; then 
             sudo mkdir /var/www/dev/
             sudo cp -r functions/f_config_nginx.html index.html
-            sudo mv index.html /var/www/dev/
-            sudo cp -r dev /etc/nginx/sites-enabled/
+            sudo mv index.html /var/www/html/
             sudo service nginx restart
         else
             mkdir /var/www/dev/
             cp -r functions/f_config_nginx.html index.html
-            mv index.html /var/www/dev/
-            cp -r dev /etc/nginx/sites-enabled/
+            mv index.html /var/www/html/
             service nginx restart
         fi
     else
