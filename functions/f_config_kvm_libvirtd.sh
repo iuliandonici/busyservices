efcfc0bad6980e4d257716322b6697a637e6abab
Iulian Donici <iuliandonici@gmail.com>
2025-08-31 16:06:28 +0200

    Reconfiguring libvirt for remote ssh.

--- functions/f_config_kvm_libvirtd.sh
+++ functions/f_config_kvm_libvirtd.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 function f_config_kvm_libirtd() {
-    echo 'listen_tls = 0' >> /etc/libvirt/libvirtd.conf
-    echo 'listen_tcp = 1' >> /etc/libvirt/libvirtd.conf
-    echo 'tcp_port = "16509"' >> /etc/libvirt/libvirtd.conf
-    echo 'listen_addr = "0.0.0.0"' >> /etc/libvirt/libvirtd.conf
-    echo 'auth_tcp = "sasl"' >> /etc/libvirt/libvirtd.conf
+    sudo systemctl stop libvirtd
+    sudo rm -rf /etc/libvirt/libvirt.conf
+    sudo cp -r functions/f_config_kvm_libvirtd /etc/libvirt/libvirt.conf
+    sudo systemctl start libvirtd
 }
