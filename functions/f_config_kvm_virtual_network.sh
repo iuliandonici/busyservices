5c4ee6beea584a1373ceb5ddbcc60c85c685c558
Iulian Donici <iuliandonici@gmail.com>
2025-08-31 20:56:26 +0200

    Reconfiguring libvirt for remote ssh.

--- functions/f_config_kvm_virtual_network.sh
+++ functions/f_config_kvm_virtual_network.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 function f_config_kvm_virtual_network() {
-    mkdir tmp
-    sudo cp -r functions/f_config_kvm_virtual_network.xml tmp/
-    sudo virsh net-define tmp/f_config_kvm_virtual_network.xml
+    sudo cp -r functions/f_config_kvm_virtual_network.xml .
+    sudo virsh net-define f_config_kvm_virtual_network.xml
     sudo virsh net-start bridged-network
     sudo virsh net-autostart bridged-network
 }
