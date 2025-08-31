a9695ee71c6915ebee573bebea51064d6fb724b8
Iulian Donici <iuliandonici@gmail.com>
2025-08-31 15:24:22 +0200

    Added correct permissions.

--- functions/f_config_kvm_sshd.sh
+++ functions/f_config_kvm_sshd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function f_config_kvm_sshd() {
-    echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-    echo "PasswordAuthentification no"  >> /etc/ssh/sshd_config
-    echo "AuthentificationMethods publickey"  >> /etc/ssh/sshd_config
+    sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+    sudo echo "PasswordAuthentification no"  >> /etc/ssh/sshd_config
+    sudo echo "AuthentificationMethods publickey"  >> /etc/ssh/sshd_config
 }
