4ca866cfeffedd514f81b175f7d76f5deac8210d
Iulian Donici <iuliandonici@gmail.com>
2025-08-31 15:49:45 +0200

    An alternative to configuring ssh preferences.

--- functions/f_config_kvm_sshd.sh
+++ functions/f_config_kvm_sshd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 function f_config_kvm_sshd() {
     sudo rm -rf /etc/ssh/sshd_config
-    sudo cp -r functions/f_config_kvm_sshd
+    sudo cp -r functions/f_config_kvm_sshd /etc/ssh/sshd_config
 }
