49cfe990d03b53c3b41cc6802b10bbb7bf28bf20
Iulian Donici <iuliandonici@gmail.com>
2025-08-31 15:40:45 +0200

    An alternative to configuring ssh preferences.

--- functions/f_config_kvm_sshd.sh
+++ functions/f_config_kvm_sshd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 function f_config_kvm_sshd() {
-    sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-    sudo echo "PasswordAuthentification no"  >> /etc/ssh/sshd_config
-    sudo echo "AuthentificationMethods publickey"  >> /etc/ssh/sshd_config
+    sudo rm -rf /etc/ssh/sshd_config
+    sudo cp -r functions/f_config_kvm_sshd
 }
