62e8b673cf9feae4ccb688194ac461db8427b9d5
Iulian Donici <iuliandonici@gmail.com>
2025-09-02 21:46:04 +0200

    Updated.

--- functions/f_config_kvm_bridged_networking.sh
+++ functions/f_config_kvm_bridged_networking.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 function f_config_kvm_bridged_networking() {
     echo " - Creating a bridged network interface so we can get the same subnet IPs as the KVM host;"
-    sudo ip link add bridge0 type bridge
+    sudo ip addr del 192.168.64.11/24 dev ens33
+    sudo ip addr flush dev ens33
+    sudo ip link add bridge0 type bridge    
     sudo ip link set ens33 master bridge0
     sudo ip address add dev bridge0 192.168.50.0/24
     sudo cp -r functions/f_config_kvm_netfilter_bridge /etc/sysctl.d/99-kvm-netfilter-bridge.conf
