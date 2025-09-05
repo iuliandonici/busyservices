719b8d3748f41f05b3d2073236430236b4e58dbb
Iulian Donici <iuliandonici@gmail.com>
2025-09-05 21:09:33 +0200

    A cronjob of restarting the networking service was added.

--- functions/f_install_server_kvm_software.sh
+++ functions/f_install_server_kvm_software.sh
@@ -5,6 +5,7 @@ function f_install_server_kvm_software() {
     source functions/f_config_kvm_sshd.sh
     source functions/f_config_kvm_libvirtd.sh
     source functions/f_config_kvm_group.sh
+    source functions/f_config_kvm_crontab.sh
     source functions/f_config_kvm_bridged_networking.sh
     source functions/f_config_kvm_virtual_network.sh
     f_update_software
