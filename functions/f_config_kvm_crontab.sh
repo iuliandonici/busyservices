e472375a0bf4f145a774a6c2d7a6f1cdb4d3bd14
Iulian Donici <iuliandonici@gmail.com>
2025-09-05 21:52:05 +0200

    A cronjob of restarting the networking service was added.

--- functions/f_config_kvm_crontab.sh
+++ functions/f_config_kvm_crontab.sh
@@ -2,7 +2,7 @@
 function f_config_kvm_crontab () {
     echo " - Adding a new crontab task:"
     crontab -l > cronjob_restart_network
-    echo "systemctl restart networking.service" >> cronjob_restart_network
+    echo "@reboot systemctl restart networking.service" >> cronjob_restart_network
     crontab cronjob_restart_network
     rm cronjob_restart_network
 }
