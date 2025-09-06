8c623c6c2bfa3ae29353aa3edb0821ec1752635a
Iulian Donici <iuliandonici@gmail.com>
2025-09-06 19:16:11 +0200

    Switched the details of the pshysical network to the virtual one.

--- functions/f_config_kvm_crontab.sh
+++ functions/f_config_kvm_crontab.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 function f_config_kvm_crontab () {
     echo " - Adding a new crontab task:"
-    echo "@reboot systemctl restart networking.service" >> cronjob_restart_network
     sudo crontab -l > cronjob_restart_network
+    echo "@reboot systemctl restart networking.service" >> cronjob_restart_network
     sudo crontab cronjob_restart_network
     # rm cronjob_restart_network
 }
