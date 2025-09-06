906cc1a65c20759689e7a383a36a741e4c4e5b92
Iulian Donici <iuliandonici@gmail.com>
2025-09-06 16:54:22 +0200

    Returned to a dhcp version.

--- show.sh
+++ show.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source functions/f_config_kvm_bridged_networking.sh
+# source functions/f_config_kvm_bridged_networking.sh
 source functions/f_config_kvm_virtual_network.sh
-f_config_kvm_bridged_networking
+source functions/f_config_kvm_crontab.sh
+# f_config_kvm_bridged_networking
 f_config_kvm_virtual_network
+f_config_kvm_crontab
