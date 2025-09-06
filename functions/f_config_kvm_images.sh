1df3baee68c833a4e87193c0725c05c8e709e6d2
Iulian Donici <iuliandonici@gmail.com>
2025-09-06 21:56:30 +0200

    Added the latest Alpine images

---
+++ functions/f_config_kvm_images.sh
@@ -0,0 +1,6 @@
+#!/bin/bashl
+function f_config_kvm_images() {
+    echo " - Getiing ISO images for our KVM;"
+    echo " - Latest ISO images for Alpine Linux:"
+    wget -r --no-parent -A 'alpine-standard-*.*.*-x86_64.iso' https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/ && rm -rf dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/alpine-standard-*.*.*_rc*-x86_64.iso dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/netboot*
+}
