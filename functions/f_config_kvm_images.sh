d72d64b59c52ca87d21449f4ca2a2a4bb4933f69
Iulian Donici <iuliandonici@gmail.com>
2025-09-07 17:36:00 +0200

    Downloading latest images for Alpine and moving them to the proper images/ folder.

--- functions/f_config_kvm_images.sh
+++ functions/f_config_kvm_images.sh
@@ -1,8 +1,13 @@
 #!/bin/bashl
 function f_config_kvm_images() {
-    echo " - Getiing ISO images for our KVM;"
-    echo " - Latest ISO images for Alpine Linux:"
-    wget -r --no-parent -X "*/*/*/*/netboot*/" -A "alpine-standard-*.*.*-x86_64.iso" -R "alpine-standard-*.*.*_rc*-x86_64.iso" https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/
-    sudo mv dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/*.iso /var/lib/libvirt/images/
+    echo " - Getting ISO images for our KVM;"
+    echo " - Getting latest ISO for Alpine Linux:"
+    wget -r --no-parent -X "*/*/*/*/netboot*/" -A "alpine-standard-*.*.*-x86_64.iso" -R "alpine-standard-*.*.*_rc*-x86_64.iso" -N https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/
+    # List the files in reverse order, remove all, except the 1st one
+    var_images_folder="dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/"
+    find $var_images_folder ! -name $(ls -h $var_images_folder | sort -nr | head -1) -type f -exec rm -f {} +
+    mkdir alpinelinux
+    sudo mv $var_images_folder/*.iso alpinelinux/
+    sudo mv alpinelinux/ /var/lib/libvirt/images/
     rm -rf dl-cdn.alpinelinux.org/
 }
