bcc4433fa2008f09f77c700426a9ad001addb468
Iulian Donici <iuliandonici@gmail.com>
2025-09-07 21:17:40 +0200

    Updated.

--- functions/f_config_kvm_crontab.sh
+++ functions/f_config_kvm_crontab.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 function f_config_kvm_crontab () {
-    echo " - Adding a new crontab task:"
-    sudo crontab -l > cronjob_restart_network
-    echo "@reboot systemctl restart networking.service" >> cronjob_restart_network
-    sudo crontab cronjob_restart_network
-    rm cronjob_restart_network
+    echo " - Checking if any crontab tasks exist:"
+    if [ $(sudo crontab -l | wc -c) -eq 0 ]; then
+        echo " - Crontab is empty so we're creating a new job for it;"
+        sudo crontab -l > cronjob_restart_network
+        echo "@reboot systemctl restart networking.service" >> cronjob_restart_network
+        sudo crontab cronjob_restart_network
+        rm cronjob_restart_network
+    else
+        echo " - Crontab is not empty so we're not adding any more jobs;"
+    fi
 }
