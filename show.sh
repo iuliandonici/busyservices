e820964327232fd383ef039ab7fbbfa9a8de118d
Iulian Donici <iuliandonici@gmail.com>
2025-09-17 11:16:06 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -1,8 +1,16 @@
-#!/bin/bashl
-function f_config_kvm_images() {
-    source functions/f_config_kvm_images_alpine.sh
-    source functions/f_config_kvm_images_ubuntu.sh
-    echo "- Getting latest ISO images for our KVM:"
-    f_config_kvm_images_alpine
-    f_config_kvm_images_ubuntu
+#!/bin/bash
+function f_config_kvm_images_ubuntu() {
+    var_f_config_kvm_images_dir="/var/lib/libvirt/images"
+    echo "- Downloading latest Ubuntu server ISO;"
+    wget -q -X "*.10" http://cdimage.ubuntu.com/releases/ -O - | awk '! ($0 % 2)' |sed -e :a -e 's/<[^>]*>//g;/</N;//ba' | grep '^[[:space:][:space:]][1-9]' | grep '.04.*' | sed 's/\///g' > ubuntuversions 
+    awk '! ($0 % 2)' ubuntuversions | sort -nr | head -1 > latestubuntuversion
+    awk '{print $1 $2}' latestubuntuversion > lastubuntuversion
+    var_latest_ubuntu_version=$(cat lastubuntuversion)
+    if ! [ -f $var_f_config_kvm_images_dir/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso ]; then
+    wget https://releases.ubuntu.com/${var_latest_ubuntu_version}/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso
+    sudo mv ubuntu*-live-server.iso $var_f_config_kvm_images_dir
+    else
+        echo "- Latest Ubuntu server ($var_latest_ubuntu_version) ISO already exists in $var_f_config_kvm_images_dir/;"
+    fi
+    rm -rf ubuntuversions latestubuntuversion lastubuntuversion
 }
