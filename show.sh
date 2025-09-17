6d083f382301e704ba589a2c76165e0e46c2dc80
Iulian Donici <iuliandonici@gmail.com>
2025-09-17 11:28:55 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -4,7 +4,7 @@ function f_config_kvm_images_ubuntu() {
     echo "- Downloading latest Ubuntu server ISO;"
     wget -q -X "*.10" http://cdimage.ubuntu.com/releases/ -O - | sed -e :a -e 's/<[^>]*>//g;/</N;//ba' | grep '^[[:space:][:space:]][1-9]' | grep '.04.*' | sed 's/\///g' > ubuntuversions 
     awk '! ($0 % 2)' ubuntuversions | sort -nr | head -1 > latestubuntuversion
-    awk '{print $1 $2}' latestubuntuversion > lastubuntuversion
+    # awk '{print $1 $2}' latestubuntuversion > lastubuntuversion
     # var_latest_ubuntu_version=$(cat lastubuntuversion)
     echo $var_latest_ubuntu_version
     # if ! [ -f $var_f_config_kvm_images_dir/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso ]; then
