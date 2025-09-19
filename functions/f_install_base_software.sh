fa0af970e9a9963889b3d910064ab502442829f5
Iulian Donici <iuliandonici@gmail.com>
2025-09-19 12:50:26 +0200

    Added bash to our list of software and removed sudo from Alpine distros.

--- functions/f_install_base_software.sh
+++ functions/f_install_base_software.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-var_install_base_software_array=("apt-utils" "lsb-release" "hostname" "wget" "rsync" "curl" "keychain" "net-tools" "unzip" "git" "nano" "ca-certificates" "curl" "gnupg" "software-properties-common" "acl")
+var_install_base_software_array=("bash" "apt-utils" "lsb-release" "hostname" "wget" "rsync" "curl" "keychain" "net-tools" "unzip" "git" "nano" "ca-certificates" "curl" "gnupg" "software-properties-common" "acl")
 function f_install_base_software() {
     source functions/f_update_software.sh
     source functions/f_config_git.sh
@@ -14,7 +14,7 @@ function f_install_base_software() {
         do
             echo "- Currently installing: $i ${var_install_base_software_array[$i]}"
             if [[ "$EUID" -ne 0 ]]; then 
-                sudo $(f_get_distro_packager) add ${var_install_base_software_array[$i]}  
+                $(f_get_distro_packager) add ${var_install_base_software_array[$i]}  
             else
                 $(f_get_distro_packager) add ${var_install_base_software_array[$i]}  
             fi
