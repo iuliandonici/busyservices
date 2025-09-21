56d7a36012bff1398f5cef813923bb001aef965a
Iulian Donici <iuliandonici@gmail.com>
2025-09-21 08:13:26 +0200

    Testing to see if we can detect the network interfaces.

--- show.sh
+++ show.sh
@@ -1,44 +1,6 @@
 #!/bin/bash
-var_install_base_software_array=("bash" "apt-utils" "lsb-release" "hostname" "wget" "rsync" "curl" "keychain" "net-tools" "unzip" "git" "nano" "ca-certificates" "curl" "gnupg" "software-properties-common" "acl")
-function f_install_base_software() {
-    source functions/f_update_software.sh
-    source functions/f_config_git.sh
-    f_update_software
-    echo "- List of base software that will be installed using $(f_get_distro_packager):"
-    for i in "${!var_install_base_software_array[@]}"
-    do
-        echo " $i ${var_install_base_software_array[$i]}"
-    done
-    if [[ $(f_get_distro_packager) == "apk" ]]; then
-        for i in "${!var_install_base_software_array[@]}"
-        do
-            echo "- Currently installing: $i ${var_install_base_software_array[$i]}"
-            if [[ "$EUID" -ne 0 ]]; then 
-                $(f_get_distro_packager) add ${var_install_base_software_array[$i]}  
-            else
-                $(f_get_distro_packager) add ${var_install_base_software_array[$i]}  
-            fi
-        done     
-    elif [[ $(f_get_distro_packager) == "dnf" || $(f_get_distro_packager) == "zypper" ]]; then
-        for i in "${!var_install_base_software_array[@]}"
-        do
-            echo "- Currently installing: $i ${var_install_base_software_array[$i]}"
-            if [[ "$EUID" -ne 0 ]]; then 
-                sudo $(f_get_distro_packager) install -y ${var_install_base_software_array[$i]}  
-            else
-                $(f_get_distro_packager) install -y ${var_install_base_software_array[$i]}  
-            fi
-        done        
-    else
-        for i in "${!var_install_base_software_array[@]}"
-        do
-            echo "- Currently installing: $i ${var_install_base_software_array[$i]}"
-            if [[ "$EUID" -ne 0 ]]; then 
-                sudo $(f_get_distro_packager) install -y ${var_install_base_software_array[$i]}  
-            else
-                $(f_get_distro_packager) install -y ${var_install_base_software_array[$i]}  
-            fi
-        done
-    fi
-    f_config_git
+function f_config_kvm_network_interfaces() {
+# Assign a variable to the network interface we find (en* or eth)
+    var_f_config_kvm_network_interfaces=$(ip a | grep -E "en|eth[[0-9]]" | sed -e 's/://g' | grep -Ev "lo|wl|virb" | awk '{print $2}')
+    echo $var_f_config_kvm_network_interfaces
 }
