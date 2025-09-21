dd5cebf016aea63ea5d06c8f0f452ed7f2e726c1
Iulian Donici <iuliandonici@gmail.com>
2025-09-21 15:28:12 +0200

    Updated.

--- functions/f_config_kvm_libvirtd.sh
+++ functions/f_config_kvm_libvirtd.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 function f_config_kvm_libvirtd() {
     echo " - Configuring KVM for remote ssh;"
-    sudo systemctl stop libvirtd
-    sudo rm -rf /etc/libvirt/libvirt.conf
-    sudo cp -r functions/f_config_kvm_libvirtd /etc/libvirt/libvirt.conf
-    sudo systemctl start libvirtd
+    if [[ "$EUID" -ne 0 ]]; then 
+        sudo systemctl stop libvirtd
+        sudo rm -rf /etc/libvirt/libvirt.conf
+        sudo cp -r functions/f_config_kvm_libvirtd /etc/libvirt/libvirt.conf
+        sudo systemctl start libvirtd
+    else
+        systemctl stop libvirtd
+        rm -rf /etc/libvirt/libvirt.conf
+        cp -r functions/f_config_kvm_libvirtd /etc/libvirt/libvirt.conf
+        systemctl start libvirtd
+    fi    
 }
