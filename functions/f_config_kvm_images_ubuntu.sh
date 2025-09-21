86f5c362cfcaa99766bf56a204663f32fd5b30ba
Iulian Donici <iuliandonici@gmail.com>
2025-09-21 13:43:22 +0200

    Removed cron jobs for now.

--- functions/f_config_kvm_images_ubuntu.sh
+++ functions/f_config_kvm_images_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function f_config_kvm_images_ubuntu() {
     var_f_config_kvm_images_dir="/var/lib/libvirt/images"
-    echo "- Downloading latest Ubuntu server ISO:"
+    echo "- then downloading latest Ubuntu server ISO:"
     wget -q -X "*.10" http://cdimage.ubuntu.com/releases/ -O - | sed -e :a -e 's/<[^>]*>//g;/</N;//ba' | grep -E '^[[:space:][:space:]][1-9].*.04.*' | sed -e 's/\///g' -e 's/ //g' > ubuntu_last 
     grep -oE '^[12468]*.[0-10][02468]*.*' ubuntu_last | sort -nr | head -1 > laststableubuntuversion
     var_latest_ubuntu_version=$(cat laststableubuntuversion)
