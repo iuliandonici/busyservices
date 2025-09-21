e114ec0c77ce1ebcaff306f11c0057d017508808
Iulian Donici <iuliandonici@gmail.com>
2025-09-21 13:06:35 +0200

    Updated.

--- functions/f_config_kvm_sshd.sh
+++ functions/f_config_kvm_sshd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function f_config_kvm_sshd() {
-    echo "- Removing, ccopying our own sshd config and restarting ssh;"
+    echo "- Removing, copying our own sshd config and restarting ssh;"
     sudo rm -rf /etc/ssh/sshd_config
     sudo cp -r functions/f_config_kvm_sshd /etc/ssh/sshd_config
     sudo systemctl restart ssh
