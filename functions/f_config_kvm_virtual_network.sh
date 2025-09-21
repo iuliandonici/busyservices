bedf29224e87b1212d037d1787f1430f5f8d3dfb
Iulian Donici <iuliandonici@gmail.com>
2025-09-21 16:47:17 +0200

    Updated.

--- functions/f_config_kvm_virtual_network.sh
+++ functions/f_config_kvm_virtual_network.sh
@@ -3,17 +3,17 @@ function f_config_kvm_virtual_network() {
     echo "- Creating a virtual bridged network;"
     if [[ $(f_get_distro_packager) == "apk" ]]; then
         if [[ "$EUID" -ne 0 ]]; then 
-            sudo rc-service virtnetworkd restart
-            sudo rc-service libvirtd restart
-            sudo rc-service virtqemud restart
             # sudo cp -r functions/f_config_kvm_virtual_network.xml .
+            sudo rc-service libvirtd restart
+            sudo rc-service virtnetworkd restart
+            sudo rc-service virtqemud restart
             sudo virsh net-define functions/f_config_kvm_virtual_network.xml
             sudo virsh net-start bridged-network
             sudo virsh net-autostart bridged-network
         else
             # cp -r functions/f_config_kvm_virtual_network.xml .
-            rc-service virtnetworkd restart
             rc-service libvirtd restart
+            rc-service virtnetworkd restart
             rc-service virtqemud restart
             virsh net-define functions/f_config_kvm_virtual_network.xml
             virsh net-start bridged-network
@@ -21,28 +21,36 @@ function f_config_kvm_virtual_network() {
         fi
     elif [[ $(f_get_distro_packager) == "dnf" || $(f_get_distro_packager) == "zypper" ]]; then
         if [[ "$EUID" -ne 0 ]]; then 
-            sudo systemctl restart virtqemud
             # sudo cp -r functions/f_config_kvm_virtual_network.xml .
+            sudo systemctl restart libvirtd
+            sudo systemctl restart virtnetworkd
+            sudo systemctl restart virtqemud
             sudo virsh net-define functions/f_config_kvm_virtual_network.xml
             sudo virsh net-start bridged-network
             sudo virsh net-autostart bridged-network
         else
-            systemctl restart virtqemud
             # cp -r functions/f_config_kvm_virtual_network.xml .
+            systemctl restart libvirtd
+            systemctl restart virtnetworkd
+            systemctl restart virtqemud
             virsh net-define functions/f_config_kvm_virtual_network.xml
             virsh net-start bridged-network
             virsh net-autostart bridged-network
         fi
     else
         if [[ "$EUID" -ne 0 ]]; then 
-            sudo systemctl restart virtqemud
             # sudo cp -r functions/f_config_kvm_virtual_network.xml .
+            sudo systemctl restart libvirtd
+            sudo systemctl restart virtnetworkd
+            sudo systemctl restart virtqemud
             sudo virsh net-define functions/f_config_kvm_virtual_network.xml
             sudo virsh net-start bridged-network
             sudo virsh net-autostart bridged-network
         else
-            systemctl restart virtqemud
             # cp -r functions/f_config_kvm_virtual_network.xml .
+            systemctl restart libvirtd
+            systemctl restart virtnetworkd
+            systemctl restart virtqemud
             virsh net-define functions/f_config_kvm_virtual_network.xml
             virsh net-start bridged-network
             virsh net-autostart bridged-network
