ddd5abad281d2d9151768fff98097f0ec1ea65af
Iulian Donici <iuliandonici@gmail.com>
2025-09-21 16:20:24 +0200

    Updated.

--- functions/f_config_kvm_virtual_network.sh
+++ functions/f_config_kvm_virtual_network.sh
@@ -2,14 +2,14 @@
 function f_config_kvm_virtual_network() {
     echo "- Creating a virtual bridged network;"
     if [[ "$EUID" -ne 0 ]]; then
-        sudo rc-service virtqemud.socket restart
+        sudo rc-service virtqemud restart
         # sudo cp -r functions/f_config_kvm_virtual_network.xml .
         sudo virsh net-define functions/f_config_kvm_virtual_network.xml
         sudo virsh net-start bridged-network
         sudo virsh net-autostart bridged-network
     else
         # cp -r functions/f_config_kvm_virtual_network.xml .
-        rc-service virtqemud.socket restart
+        rc-service virtqemud restart
         virsh net-define functions/f_config_kvm_virtual_network.xml
         virsh net-start bridged-network
         virsh net-autostart bridged-network
