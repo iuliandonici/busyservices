9b37f86a9f3e391289b589dd908c7d1ba9569ce2
Iulian Donici <iuliandonici@gmail.com>
2025-09-21 08:20:54 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function f_config_kvm_network_interfaces() {
 # Assign a variable to the network interface we find (en* or eth)
-    var_f_config_kvm_network_interfaces=$(ip a | grep -E "en|eth[[0-9]]" | sed -e 's/://g' | grep -Ev "lo|wl|virb" | awk '{print $2}')
+    var_f_config_kvm_network_interfaces=$(ip a | grep -E "en|eth[[0-9]]" | sed -e 's/://g' | grep -Ev "lo|wl|br|virb" | awk '{print $2}')
     echo $var_f_config_kvm_network_interfaces
 }
 f_config_kvm_network_interfaces
