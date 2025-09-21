f9372e3b588b8e05cd5ad68e301efe55236f97f2
Iulian Donici <iuliandonici@gmail.com>
2025-09-21 15:29:10 +0200

    Updated.

--- functions/f_config_kvm_group.sh
+++ functions/f_config_kvm_group.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 function f_config_kvm_group() {
     echo "- Creating users and groups for KVM;"
-    sudo usermod -aG kvm,libvirt $USER
-    newgrp libvirt
+    if [[ "$EUID" -ne 0 ]]; then 
+        sudo usermod -aG kvm,libvirt $USER
+        newgrp libvirt
+    else
+        usermod -aG kvm,libvirt $USER
+        newgrp libvirt
+    fi    
 }
