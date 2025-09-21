003b802585f52e6cc652177e443a7c6707121450
Iulian Donici <iuliandonici@gmail.com>
2025-09-21 17:19:43 +0200

    Updated.

--- functions/f_config_kvm_virtual_network.sh
+++ functions/f_config_kvm_virtual_network.sh
@@ -4,18 +4,23 @@ function f_config_kvm_virtual_network() {
     if [[ $(f_get_distro_packager) == "apk" ]]; then
         if [[ "$EUID" -ne 0 ]]; then 
             # sudo cp -r functions/f_config_kvm_virtual_network.xml .
+            sudo rc-update add libvirtd
             sudo rc-service libvirtd restart
+            sudo rc-update add virtnedworkd
             sudo rc-service virtnetworkd restart
+            sudo rc-update add virtqemud
             sudo rc-service virtqemud restart
             sudo virsh net-define functions/f_config_kvm_virtual_network.xml
             sudo virsh net-start bridged-network
             sudo virsh net-autostart bridged-network
         else
             # cp -r functions/f_config_kvm_virtual_network.xml .
+            rc-update add libvirtd
             rc-service libvirtd restart
+            rc-update add virtnedworkd
             rc-service virtnetworkd restart
-            rc-service virtqemud restart
-            virsh net-define functions/f_config_kvm_virtual_network.xml
+            rc-update add virtqemud
+            rc-service virtqemud restart            virsh net-define functions/f_config_kvm_virtual_network.xml
             virsh net-start bridged-network
             virsh net-autostart bridged-network
         fi
