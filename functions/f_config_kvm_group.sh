b03c9e89a474a46defb07c056fc704376961312f
Iulian Donici <iuliandonici@gmail.com>
2025-09-21 20:48:04 +0200

    Updated.

--- functions/f_config_kvm_group.sh
+++ functions/f_config_kvm_group.sh
@@ -3,18 +3,18 @@ function f_config_kvm_group() {
     echo "- Creating users and groups for KVM;"
     if [[ $(f_get_distro_packager) == "apk" ]]; then
         if [[ "$EUID" -ne 0 ]]; then 
-            sudo addgroup $USER libvirt
             sudo addgroup $USER kvm
+            sudo addgroup $USER libvirt
             sudo addgroup $USER qemu
-            sudo addgroup busyneo libvirt
             sudo addgroup busyneo kvm
+            sudo addgroup busyneo libvirt
             sudo addgroup busyneo qemu
         else
-            addgroup $USER libvirt
             addgroup $USER kvm
+            addgroup $USER libvirt
             addgroup $USER qemu
-            addgroup busyneo libvirt
             addgroup busyneo kvm
+            addgroup busyneo libvirt
             addgroup busyneo qemu
         fi    
     # usermod -aG kvm,libvirt $USER
