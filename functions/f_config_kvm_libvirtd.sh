9e37a0dcbbb9411ff96e29bd9cc1f7490a8c020d
Iulian Donici <iuliandonici@gmail.com>
2025-09-25 18:05:06 +0200

    Updated.

--- functions/f_config_kvm_libvirtd.sh
+++ functions/f_config_kvm_libvirtd.sh
@@ -4,13 +4,13 @@ function f_config_kvm_libvirtd() {
     if [[ $(f_get_distro_packager) == "apk" ]]; then
         if [[ "$EUID" -ne 0 ]]; then 
             sudo rc-service libvirtd stop
-            # sudo rm -rf /etc/libvirt/libvirt.conf
-            # sudo cp -r functions/f_config_kvm_libvirtd /etc/libvirt/libvirt.conf
+            sudo rm -rf /etc/libvirt/libvirt.conf
+            sudo cp -r functions/f_config_kvm_libvirtd /etc/libvirt/libvirt.conf
             sudo rc-service libvirtd start
         else
             rc-service libvirtd stop
-            # rm -rf /etc/libvirt/libvirt.conf
-            # cp -r functions/f_config_kvm_libvirtd /etc/libvirt/libvirt.conf
+            rm -rf /etc/libvirt/libvirt.conf
+            cp -r functions/f_config_kvm_libvirtd /etc/libvirt/libvirt.conf
             rc-service libvirtd start
         fi
     elif [[ $(f_get_distro_packager) == "dnf" || $(f_get_distro_packager) == "zypper" ]]; then
