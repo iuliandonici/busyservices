73a37e07949077029b31cc0519f876c9124a6f85
Iulian Donici <iuliandonici@gmail.com>
2025-09-28 13:24:26 +0200

    Testing a new MAC address for the bridge0 interface.

--- functions/f_config_kvm_bridged_networking.sh
+++ functions/f_config_kvm_bridged_networking.sh
@@ -5,9 +5,10 @@ function f_config_kvm_bridged_networking() {
     macaddr=$(echo $FQDN|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
     if [[ "$EUID" -ne 0 ]]; then 
         sudo cp -r functions/f_config_kvm_netfilter_bridge /etc/sysctl.d/99-kvm-netfilter-bridge.conf
-        sudo ip link add bridge0 type bridge
+        sudo ip link add name bridge0 address $macaddr up type bridge
+        # sudo ip link add bridge0 type bridge
         # Set the newly generated MAC address to the bridge0 interface
-        sudo ip link set dev bridge0 address $macaddr
+        # sudo ip link set dev bridge0 address $macaddr
         # sudo ip link set ens33 master bridge0
         sudo ip address add dev bridge0 192.168.50.0/24
         sudo modprobe br_netfilter
@@ -16,8 +17,8 @@ function f_config_kvm_bridged_networking() {
         sudo sysctl -p /etc/sysctl.d/99-kvm-netfilter-bridge.conf
     else
         cp -r functions/f_config_kvm_netfilter_bridge /etc/sysctl.d/99-kvm-netfilter-bridge.conf
-        ip link add bridge0 type bridge
-        ip link set dev bridge0 address $macaddr
+        ip link add name bridge0 address $macaddr up type bridge
+        # ip link set dev bridge0 address $macaddr
         # sudo ip link set ens33 master bridge0
         ip address add dev bridge0 192.168.50.0/24
         modprobe br_netfilter
