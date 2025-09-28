6a314199305c494076f5ce002c32cef97a3b0e36
Iulian Donici <iuliandonici@gmail.com>
2025-09-28 12:52:43 +0200

    Testing a new MAC address for the bridge0 interface.

--- functions/f_config_kvm_group.sh
+++ functions/f_config_kvm_group.sh
@@ -5,23 +5,19 @@ function f_config_kvm_group() {
         if [[ "$EUID" -ne 0 ]]; then 
             sudo addgroup $USER kvm
             sudo addgroup $USER libvirt
-            sudo addgroup $USER sudo
             sudo addgroup $USER qemu
             sudo addgroup $USER wheel
             sudo addgroup busyneo kvm
             sudo addgroup busyneo libvirt
-            sudo addgroup busyneo sudo
             sudo addgroup busyneo qemu
             sudo addgroup busyneo wheel
         else
             addgroup $USER kvm
             addgroup $USER libvirt
-            addgroup $USER sudo
             addgroup $USER qemu
             addgroup $USER wheel
             addgroup busyneo kvm
             addgroup busyneo libvirt
-            addgroup busyneo sudo
             addgroup busyneo qemu
             addgroup busyneo wheel
         fi    
