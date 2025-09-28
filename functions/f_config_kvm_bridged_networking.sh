208d1df9a4511d95fcf2212b36bd9af99c28c1bf
Iulian Donici <iuliandonici@gmail.com>
2025-09-28 13:02:24 +0200

    Testing a new MAC address for the bridge0 interface.

--- functions/f_config_kvm_bridged_networking.sh
+++ functions/f_config_kvm_bridged_networking.sh
@@ -3,7 +3,7 @@ function f_config_kvm_bridged_networking() {
     echo "- Creating a bridged network interface so we can get the same subnet IPs as the KVM host;"
     if [[ "$EUID" -ne 0 ]]; then 
         sudo cp -r functions/f_config_kvm_netfilter_bridge /etc/sysctl.d/99-kvm-netfilter-bridge.conf
-        sudo ip link add bridge0 type bridge
+        sudo ip link add name bridge0 address 12:34:56:78:9a:bc up type bridge
         # sudo ip link set ens33 master bridge0
         sudo ip address add dev bridge0 192.168.50.0/24
         sudo modprobe br_netfilter
@@ -12,7 +12,7 @@ function f_config_kvm_bridged_networking() {
         sudo sysctl -p /etc/sysctl.d/99-kvm-netfilter-bridge.conf
     else
         cp -r functions/f_config_kvm_netfilter_bridge /etc/sysctl.d/99-kvm-netfilter-bridge.conf
-        ip link add bridge0 type bridge
+        ip link add name bridge0 address 12:34:56:78:9a:bc up type bridge
         # sudo ip link set ens33 master bridge0
         ip address add dev bridge0 192.168.50.0/24
         modprobe br_netfilter
