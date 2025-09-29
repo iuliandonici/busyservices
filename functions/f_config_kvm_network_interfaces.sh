c80458691559e6393654ac9a032509ea0b2b3554
Iulian Donici <iuliandonici@gmail.com>
2025-09-29 15:19:26 +0200

    Updated.

--- functions/f_config_kvm_network_interfaces.sh
+++ functions/f_config_kvm_network_interfaces.sh
@@ -3,12 +3,13 @@ function f_config_kvm_network_interfaces() {
     echo "- Creating the network interfaces yaml file;"
     echo "- currently generating a random MAC address for the bridge interface;"
     var_f_config_kvm_network_interfaces_macaddr=$(echo $FQDN|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
+    echo "- currently removing the previous network interfaces file being root or not;"
     if [[ "$EUID" -ne 0 ]]; then
         sudo rm -rf /etc/network/interfaces
     else 
         rm -rf /etc/network/interfaces
     fi
-# Assign a variable to the network interface we find (en* or eth)
+    echo "- and assigning a variable to the network interface we find (en* or eth);"
     var_f_config_kvm_network_interfaces=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $2}' | sed -e 's/://g')
     echo -e "# interfaces(5) file used by ifup(8) and ifdown(8)
 # Include files from /etc/network/interfaces.d:
@@ -20,7 +21,7 @@ iface lo inet loopback \n
 # should be configured manually, to avoid conflicts with NetworkManager" >> config_kvm_network_interfaces.yaml
 # If the network interface variable isn't empty, then apply the default config
     if [ ! -z "${var_f_config_kvm_network_interfaces}" ]; then
-        echo "- currently the network interface variable isn't empty so we're going tu use a default yaml config;"
+        echo "- but the network interface variable isn't empty so we're going tu use a default yaml config;"
         echo -e "auto $var_f_config_kvm_network_interfaces
 iface $var_f_config_kvm_network_interfaces inet manual \n" >> config_kvm_network_interfaces.yaml
         echo "# The bridge0 bridge settings
@@ -39,7 +40,7 @@ iface bridge0 inet dhcp
    bridge_fd       0" >> config_kvm_network_interfaces.yaml
 # If the network interface variable is empty then create a default eth0 interface
     else 
-        echo "- currently the network interface variable is empty so we're creating eth0 as a default one;"
+        echo "- and since the network interface variable is empty, we're creating eth0 as a default one;"
         var_f_config_kvm_network_interfaces="eth0"
         echo -e "auto $var_f_config_kvm_network_interfaces
 iface $var_f_config_kvm_network_interfaces inet manual \n" >> config_kvm_network_interfaces.yaml
