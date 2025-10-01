1dca3d2483364e64cddd18d9e9a6513cc0b67227
Iulian Donici <iuliandonici@gmail.com>
2025-10-01 10:32:41 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 function f_install_kde() {
-    sudo apk add setup-xorg-base plasma elogind polkit-elogind kde-applications font-terminus font-inconsolata font-dejavu font-noto font-noto-cjk font-awesome font-noto-extra font-liberation python3
+    setup-xorg-base
+    sudo apk add  plasma elogind polkit-elogind kde-applications font-terminus font-inconsolata font-dejavu font-noto font-noto-cjk font-awesome font-noto-extra font-liberation python3
     sudo rc-update add sddm
     git clone git@github.com:iuliandonici/busychrome-audio.git
     cd busychrome-audio/
