dcfbd5c95056513f2ed8c91e4e713af7d97aa8bc
Iulian Donici <iuliandonici@gmail.com>
2025-10-02 14:38:57 +0200

    Updated.

--- functions/f_install_busychrome_audio.sh
+++ functions/f_install_busychrome_audio.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 function f_install_busychrome_audio() {
+        echo "- and currently installing audio;"
         git clone git@github.com:iuliandonici/busychrome-audio.git
         cd busychrome-audio/
         ./setup-audio --force-avs-install
