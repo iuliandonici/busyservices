e697fdb8bf99b36cb156908b5b6441f19314f845
Iulian Donici <iuliandonici@gmail.com>
2025-10-02 16:46:31 +0200

    Updated.

--- functions/f_install_kde_requirements.sh
+++ functions/f_install_kde_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-var_install_kde_software_array=("plasma elogind" "polkit-elogind" "kde-applications-admin" "kde-applications-network" "kde-applications-utils" "font-terminus" "font-inconsolata" "font-dejavu" "font-noto" "font-noto-cjk" "font-awesome" "font-noto-extra" "font-liberation" "python3" "networkmanager" "networkmanager-tui" "networkmanager-cli" "plasma-nm" "network-manager-applet" "networkmanager-wifi")
+var_install_kde_software_array=("plasma elogind" "polkit-elogind" "kde-applications-admin" "kde-applications-network" "kde-applications-utils" "kde-applications-base" "font-terminus" "font-inconsolata" "font-dejavu" "font-noto" "font-noto-cjk" "font-awesome" "font-noto-extra" "font-liberation" "python3" "networkmanager" "networkmanager-tui" "networkmanager-cli" "plasma-nm" "network-manager-applet" "networkmanager-wifi")
 function f_install_kde_requirements() {
     source functions/f_update_software.sh
     source functions/f_config_kde.sh
