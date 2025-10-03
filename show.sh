156bd1a97c7ecdbca03df1c271d6e696c98a9a03
Iulian Donici <iuliandonici@gmail.com>
2025-10-03 19:42:17 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -6,6 +6,8 @@ function f_install_bluetooth() {
     sudo adduser $USER lp
     sudo rc-service bluetooth start
     sudo rc-update add bluetooth default
-    sudo sed -e "s/#Experimental = false/Experimental = true/g" -e "s/#AutoEnable=true/AutoEnable=false/g" -i /etc/bluetooth/main.conf
+    var_install_bluetooth_name="$(hostname)-bt"
+    sudo sed -e "s/#Experimental = false/Experimental = true/g" -e "s/#AutoEnable=true/AutoEnable=false/g" -e "s/#Name = BlueZ/Name = $var_install_bluetooth_name" -i /etc/bluetooth/main.conf
+    
 }
 f_install_bluetooth
