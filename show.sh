9039f2fd21ef315ad5157e5ca34f3e49326889a3
Iulian Donici <iuliandonici@gmail.com>
2025-10-03 19:27:41 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -6,5 +6,6 @@ function f_install_bluetooth() {
     sudo adduser $USER lp
     sudo rc-service bluetooth start
     sudo rc-update add bluetooth default
+    sudo sed -e "s/#Experimental = false/Experimental = true/g" -e "s/#AutoEnable=true/AutoEnable=false/g" -i /etc/bluetooth/main.conf
 }
 f_install_bluetooth
