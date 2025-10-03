b642ca5406e93d3fbcb0f88d968a09abecd9f154
Iulian Donici <iuliandonici@gmail.com>
2025-10-03 18:28:17 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -2,7 +2,7 @@
 function f_install_busychrome_audio() {
         echo "- and currently installing audio;"
         git clone git@github.com:iuliandonici/busychrome-audio.git
-        cd busycrohome-audio
+        cd busychrome-audio
         ./setup-audio
         cd ../
         rm -rf busychrome-audio
