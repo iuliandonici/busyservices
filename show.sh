d4c5b2df45558a818f8567e927c70e77b705d7b9
Iulian Donici <iuliandonici@gmail.com>
2025-10-10 18:52:02 +0200

    Updated.

--- show.sh
+++ show.sh
@@ -1,43 +1,24 @@
 #!/bin/bash
-function f_config_kvm_group() {
-    echo "- Creating users and groups for KVM;"
-    source functions/f_get_distro_packager.sh
-    if [[ $(f_get_distro_packager) == "apk" ]]; then
-        if [[ "$EUID" -ne 0 ]]; then 
-            sudo addgroup $USER kvm
-            sudo addgroup $USER libvirt
-            sudo addgroup $USER wheel
-            sudo addgroup $USER qemu
-            sudo addgroup busyneo kvm
-            sudo addgroup busyneo libvirt
-            sudo addgroup busyneo wheel
-            sudo addgroup busyneo qemu
-        else
-            addgroup $USER kvm
-            addgroup $USER libvirt
-            addgroup $USER wheel
-            addgroup $USER qemu
-            addgroup busyneo kvm
-            addgroup busyneo libvirt
-            addgroup busyneo wheel
-            addgroup busyneo qemu
-        fi    
-    elif [[ $(f_get_distro_packager) == "dnf" || $(f_get_distro_packager) == "zypper" ]]; then
-        if [[ "$EUID" -ne 0 ]]; then 
-            sudo usermod -aG kvm,libvirt,wheel,qemu $USER
-            sudo newgrp libvirt
-        else
-            usermod -aG kvm,libvirt,wheel,qemu $USER
-            newgrp libvirt
-        fi
+function f_config_kvm_bridged_networking() {
+    echo "- Creating a bridged network interface so we can get the same subnet IPs as the KVM host;"
+    if [[ "$EUID" -ne 0 ]]; then 
+        sudo cp -r functions/f_config_kvm_netfilter_bridge /etc/sysctl.d/99-kvm-netfilter-bridge.conf
+        sudo ip link add bridge0 type bridge
+        # sudo ip link set ens33 master bridge0
+        sudo ip address add dev bridge0 192.168.50.0/24
+        sudo modprobe br_netfilter
+        sudo echo "br_netfilter" >> br_netfilter.conf
+        sudo mv br_netfilter.conf /etc/modules-load.d/br_netfilter.conf
+        sudo sysctl -p /etc/sysctl.d/99-kvm-netfilter-bridge.conf
     else
-        if [[ "$EUID" -ne 0 ]]; then 
-            sudo usermod -aG kvm,libvirt $USER
-            newgrp libvirt
-        else
-            usermod -aG kvm,libvirt $USER
-            newgrp libvirt
-        fi
+        cp -r functions/f_config_kvm_netfilter_bridge /etc/sysctl.d/99-kvm-netfilter-bridge.conf
+        ip link add bridge0 type bridge
+        # sudo ip link set ens33 master bridge0
+        ip address add dev bridge0 192.168.50.0/24
+        modprobe br_netfilter
+        echo "br_netfilter" >> br_netfilter.conf
+        mv br_netfilter.conf /etc/modules-load.d/br_netfilter.conf
+        sysctl -p /etc/sysctl.d/99-kvm-netfilter-bridge.conf
     fi
 }
-f_config_kvm_group
+f_config_kvm_bridged_networking
