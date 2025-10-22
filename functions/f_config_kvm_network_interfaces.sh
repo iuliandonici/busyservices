4404236599b4b8fa69da250016476e2d48339d85
Iulian Donici <iuliandonici@gmail.com>
2025-10-22 14:27:52 +0200

    Updated.

--- functions/f_config_kvm_network_interfaces.sh
+++ functions/f_config_kvm_network_interfaces.sh
@@ -15,6 +15,9 @@ function f_config_kvm_network_interfaces() {
     echo "- currently generating variables for network interfaces and their status;"
     var_f_config_kvm_network_wired_interfaces=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $2}' | sed -e 's/://g')
     var_f_config_kvm_network_wired_interfaces_status=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $9}' | sed -e 's/://g')
+    if [[ $var_f_config_kvm_network_wired_interfaces_status == "bridge0" ]]; then
+        var_f_config_kvm_network_wired_interfaces_status=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $11}' | sed -e 's/://g')
+    fi  
     var_f_config_kvm_network_wireless_interfaces=$(ip a | grep -E "wl.*:" | awk '{print $2}' | sed -e 's/://g')
     var_f_config_kvm_network_wireless_interfaces_status=$(ip a | grep -E "wl.*:" | awk '{print $9}' | sed -e 's/://g')
     echo "- currently removing the previous network interfaces file being root or not;"
