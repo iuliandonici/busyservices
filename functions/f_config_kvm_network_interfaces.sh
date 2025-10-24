34346952123df4fed00ed6fe302bc8e87f4545c4
Iulian Donici <iuliandonici@gmail.com>
2025-10-24 16:21:31 +0200

    Updated.

--- functions/f_config_kvm_network_interfaces.sh
+++ functions/f_config_kvm_network_interfaces.sh
@@ -5,13 +5,12 @@ function f_config_kvm_network_interfaces() {
     echo "- currently generating a random MAC address for the bridge interface;"
     var_f_config_kvm_network_interfaces_macaddr=$(echo $FQDN|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
     var_f_config_kvm_network_wired_interfaces=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $2}' | sed -e 's/://g')
-    echo "- currently trying, just in case, to bring $var_f_config_kvm_network_wired_interfaces online;"
+    echo "- currently trying, just in case, to bring $var_f_config_kvm_network_wired_interfaces online in 5s;"
     if [[ "$EUID" -ne 0 ]]; then
         sudo ip link set dev $var_f_config_kvm_network_wired_interfaces up
     else
         ip link set dev $var_f_config_kvm_network_wired_interfaces up
     fi
-    enable sleep
     sleep 5s
     echo "- currently generating variables for network interfaces status;"
     var_f_config_kvm_network_wired_interfaces_status=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $9}' | sed -e 's/://g')
