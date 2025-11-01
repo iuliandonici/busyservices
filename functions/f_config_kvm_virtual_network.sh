b29287852cad3d09f7d10f58273c3854c584bfce
Iulian Donici <iuliandonici@gmail.com>
2025-11-01 11:05:41 +0100

    Updated.

--- functions/f_config_kvm_virtual_network.sh
+++ functions/f_config_kvm_virtual_network.sh
@@ -2,88 +2,92 @@
 function f_config_kvm_virtual_network() {
     echo "- Creating a virtual bridged network:"
     if [[ $(f_get_distro_packager) == "apk" ]]; then
-        if [[ "$EUID" -ne 0 ]]; then 
-            # Allow VMs to start and stop when the host does so
-            sudo rc-update add libvirt-guests
-            # sudo cp -r functions/f_config_kvm_virtual_network.xml .
-            sudo mkdir -p /etc/polkit-1/localauthority/50-local.d/
-            sudo echo "[Remote libvirt SSH access]
- Identity=unix-group:libvirt
- Action=org.libvirt.unix.manage
- ResultAny=yes
- ResultInactive=yes
- ResultActive=yes" > 50-libvirt-ssh-remote-access-policy.pkla
-            sudo mv 50-libvirt-ssh-remote-access-policy.pkla /etc/polkit-1/localauthority/50-local.d/
-            sudo rc-update add dbus
-            sudo rc-update add dbus boot
-            sudo rc-service dbus start
-            sudo rc-update add libvirtd
-            sudo rc-service libvirtd start
-            sudo rc-service polkit restart
-            sudo echo "tap" >> tap.conf
-            sudo mv tap.conf /etc/modules-load.d/
-            sudo cat /etc/modules | grep tan || sudo echo "tan" >> /etc/modules
-            sudo modprobe tun
-            sudo echo "tun" >> tun.conf
-            sudo mv tun.conf /etc/modules-load.d/
-            sudo cat /etc/modules | grep tun || doas echo "tun" >> /etc/modules
-            sudo echo "allow bridge0" > bridge.conf
-            sudo mv bridge.conf /etc/qemu/
-            sudo echo "# Enable bridge forwarding.
-#net.ipv4.conf.bridge0_bc_forwarding=1
-net.ipv4.ip_forward=1
-# Ignore iptables on bridge interfaces.
-net.bridge.bridge-nf-call-iptables=0" > bridging.conf
-            sudo mv bridging.conf /etc/sysctl.d/
-            # sudo rc-service libvirtd
-            # sudo rc-update add virtnetworkd
-            # sudo rc-service virtnetworkd restart
-            # sudo rc-update add virtqemud
-            # sudo rc-service virtqemud restart
-            sudo virsh net-define functions/f_config_kvm_virtual_network.xml
-            sudo virsh net-start bridged-network
-            sudo virsh net-autostart bridged-network
-        else
-            # Allow VMs to start and stop when the host does so
-            rc-update add libvirt-guests
-            # cp -r functions/f_config_kvm_virtual_network.xml 
-            mkdir -p /etc/polkit-1/localauthority/50-local.d/
-            echo "[Remote libvirt SSH access]
- Identity=unix-group:libvirt
- Action=org.libvirt.unix.manage
- ResultAny=yes
- ResultInactive=yes
- ResultActive=yes" > 50-libvirt-ssh-remote-access-policy.pkla
-            mv 50-libvirt-ssh-remote-access-policy.pkla /etc/polkit-1/localauthority/50-local.d/
-            rc-update add dbus
-            rc-update add dbus boot
-            rc-service dbus start
-            rc-update add libvirtd
-            rc-service libvirtd start
-            rc-service polkit restart
-            modprobe tun
-            echo "tun" >> tun.conf
-            mv tun.conf /etc/modules-load.d/
-            cat /etc/modules | grep tun || echo tun >> /etc/modules
-            echo "tap" >> tap.conf
-            mv tap.conf /etc/modules-load.d/
-            cat /etc/modules | grep tan || echo tan >> /etc/modules
-            echo "allow bridge0" > bridge.conf
-            mv bridge.conf /etc/qemu/
-            echo "# Enable bridge forwarding.
-#net.ipv4.conf.bridge0_bc_forwarding=1
-net.ipv4.ip_forward=1
-# Ignore iptables on bridge interfaces.
-net.bridge.bridge-nf-call-iptables=0" > bridging.conf
-            mv bridging.conf /etc/sysctl.d/
-            # rc-service libvirtd
-            # rc-update add virtnetworkd
-            # rc-service virtnetworkd restart
-            # rc-update add virtqemud
-            # rc-service virtqemud restart
-            virsh net-define functions/f_config_kvm_virtual_network.xml
-            virsh net-start bridged-network
-            virsh net-autostart bridged-network
+        if ([[ -z $var_f_config_kvm_network_wired_interfaces ]] && [[ ! -z $var_f_config_kvm_network_wireless_interfaces ]]); then
+            echo "- but the wired  network interface is empty while the wireless ($var_f_config_kvm_network_wireless_interfaces) one isn't which means we're going to use NAT as our default virtual network;"
+        else      
+            if [[ "$EUID" -ne 0 ]]; then 
+                # Allow VMs to start and stop when the host does so
+                sudo rc-update add libvirt-guests
+                # sudo cp -r functions/f_config_kvm_virtual_network.xml .
+                sudo mkdir -p /etc/polkit-1/localauthority/50-local.d/
+                sudo echo "[Remote libvirt SSH access]
+    Identity=unix-group:libvirt
+    Action=org.libvirt.unix.manage
+    ResultAny=yes
+    ResultInactive=yes
+    ResultActive=yes" > 50-libvirt-ssh-remote-access-policy.pkla
+                sudo mv 50-libvirt-ssh-remote-access-policy.pkla /etc/polkit-1/localauthority/50-local.d/
+                sudo rc-update add dbus
+                sudo rc-update add dbus boot
+                sudo rc-service dbus start
+                sudo rc-update add libvirtd
+                sudo rc-service libvirtd start
+                sudo rc-service polkit restart
+                sudo echo "tap" >> tap.conf
+                sudo mv tap.conf /etc/modules-load.d/
+                sudo cat /etc/modules | grep tan || sudo echo "tan" >> /etc/modules
+                sudo modprobe tun
+                sudo echo "tun" >> tun.conf
+                sudo mv tun.conf /etc/modules-load.d/
+                sudo cat /etc/modules | grep tun || doas echo "tun" >> /etc/modules
+                sudo echo "allow bridge0" > bridge.conf
+                sudo mv bridge.conf /etc/qemu/
+                sudo echo "# Enable bridge forwarding.
+    #net.ipv4.conf.bridge0_bc_forwarding=1
+    net.ipv4.ip_forward=1
+    # Ignore iptables on bridge interfaces.
+    net.bridge.bridge-nf-call-iptables=0" > bridging.conf
+                sudo mv bridging.conf /etc/sysctl.d/
+                # sudo rc-service libvirtd
+                # sudo rc-update add virtnetworkd
+                # sudo rc-service virtnetworkd restart
+                # sudo rc-update add virtqemud
+                # sudo rc-service virtqemud restart
+                sudo virsh net-define functions/f_config_kvm_virtual_network.xml
+                sudo virsh net-start bridged-network
+                sudo virsh net-autostart bridged-network
+            else
+                # Allow VMs to start and stop when the host does so
+                rc-update add libvirt-guests
+                # cp -r functions/f_config_kvm_virtual_network.xml 
+                mkdir -p /etc/polkit-1/localauthority/50-local.d/
+                echo "[Remote libvirt SSH access]
+    Identity=unix-group:libvirt
+    Action=org.libvirt.unix.manage
+    ResultAny=yes
+    ResultInactive=yes
+    ResultActive=yes" > 50-libvirt-ssh-remote-access-policy.pkla
+                mv 50-libvirt-ssh-remote-access-policy.pkla /etc/polkit-1/localauthority/50-local.d/
+                rc-update add dbus
+                rc-update add dbus boot
+                rc-service dbus start
+                rc-update add libvirtd
+                rc-service libvirtd start
+                rc-service polkit restart
+                modprobe tun
+                echo "tun" >> tun.conf
+                mv tun.conf /etc/modules-load.d/
+                cat /etc/modules | grep tun || echo tun >> /etc/modules
+                echo "tap" >> tap.conf
+                mv tap.conf /etc/modules-load.d/
+                cat /etc/modules | grep tan || echo tan >> /etc/modules
+                echo "allow bridge0" > bridge.conf
+                mv bridge.conf /etc/qemu/
+                echo "# Enable bridge forwarding.
+    #net.ipv4.conf.bridge0_bc_forwarding=1
+    net.ipv4.ip_forward=1
+    # Ignore iptables on bridge interfaces.
+    net.bridge.bridge-nf-call-iptables=0" > bridging.conf
+                mv bridging.conf /etc/sysctl.d/
+                # rc-service libvirtd
+                # rc-update add virtnetworkd
+                # rc-service virtnetworkd restart
+                # rc-update add virtqemud
+                # rc-service virtqemud restart
+                virsh net-define functions/f_config_kvm_virtual_network.xml
+                virsh net-start bridged-network
+                virsh net-autostart bridged-network
+            fi
         fi
     elif [[ $(f_get_distro_packager) == "dnf" || $(f_get_distro_packager) == "zypper" ]]; then
         if [[ "$EUID" -ne 0 ]]; then 
