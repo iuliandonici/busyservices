d05054698af81c9c4c5687f1f3c82e7b75b7d523
Iulian Donici <iuliandonici@gmail.com>
2025-11-02 13:36:38 +0100

    Updated.

--- show.sh
+++ show.sh
@@ -11,27 +11,31 @@ function f_install_lite_xl() {
         echo "- currently giving currrent user permissions to the Alpine Linux repositories file (/etc/apk/repositories);"
         doas chown -R $USER:$USER /etc/apk/repositories
         echo "- currently adding the the edge testing Alpine repository;"
-        doas echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+        sudo echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+      fi
         sudo apk update
         sudo apk add lite-xl
         echo "- currently adding the lite-xl plugin manager called lpm;"
-        wget https://github.com/lite-xl/lite-xl-plugin-manager/releases/download/latest/lpm.`uname -m | sed 's/arm64/aarch64/'`-`uname | tr '[:upper:]' '[:lower:]'` -O lpm && chmod +x lpm
-        ./lpm install plugin_manager --assume-yes
         sudo mv lpm /usr/bin/
-        lpm plugin install autocomplete autosave autowrap editorconfig formatter gitstatus indentguide language_cmake language_containerfile language_htaccess language_sh language_csharp language_env language_fstab language_go language_ignore language_ini language_json language_nginx language_objc language_ssh_config language_yaml lsp plugin_manager restoretabs search_ui select_colorscheme selectionhighlight settings snippets tab_switcher updatechecker
-      fi
-    else
-        echo "- currently giving currrent user permissions to the Alpine Linux repositories file (/etc/apk/repositories);"
-        chown -R $USER:$USER /etc/apk/repositories
-        echo "- currently adding the the edge testing Alpine repository;"
-        echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-        apk update
-        apk add lite-xl
-        echo "- currently adding the lite-xl plugin manager called lpm;"
         wget https://github.com/lite-xl/lite-xl-plugin-manager/releases/download/latest/lpm.`uname -m | sed 's/arm64/aarch64/'`-`uname | tr '[:upper:]' '[:lower:]'` -O lpm && chmod +x lpm
         ./lpm install plugin_manager --assume-yes
-        mv lpm /usr/bin/
-        lpm plugin install autocomplete autosave autowrap editorconfig formatter gitstatus indentguide language_cmake language_containerfile language_htaccess language_sh language_csharp language_env language_fstab language_go language_ignore language_ini language_json language_nginx language_objc language_ssh_config language_yaml lsp plugin_manager restoretabs search_ui select_colorscheme selectionhighlight settings snippets tab_switcher updatechecker
+        lpm plugin install autocomplete autosave autowrap editorconfig formatter gitstatus indentguide language_cmake language_containerfile language_htaccess language_sh language_csharp language_env language_fstab language_go language_ignore language_ini language_json language_nginx language_objc language_ssh_config language_yaml lsp plugin_manager restoretabs search_ui select_colorscheme selectionhighlight settings snippets tab_switcher
+    else
+      if grep -wq "edge/testing" /etc/apk/repositories; then
+        echo "- but the edge testing Alpine repository exists already;"
+      else
+        echo "- currently giving currrent user permissions to the Alpine Linux repositories file (/etc/apk/repositories);"
+        doas chown -R $USER:$USER /etc/apk/repositories
+        echo "- currently adding the the edge testing Alpine repository;"
+        sudo echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+      fi
+      apk update
+      apk add lite-xl
+      echo "- currently adding the lite-xl plugin manager called lpm;"
+      wget https://github.com/lite-xl/lite-xl-plugin-manager/releases/download/latest/lpm.`uname -m | sed 's/arm64/aarch64/'`-`uname | tr '[:upper:]' '[:lower:]'` -O lpm && chmod +x lpm
+      ./lpm install plugin_manager --assume-yes
+      mv lpm /usr/bin/
+      lpm plugin install autocomplete autosave autowrap editorconfig formatter gitstatus indentguide language_cmake language_containerfile language_htaccess language_sh language_csharp language_env language_fstab language_go language_ignore language_ini language_json language_nginx language_objc language_ssh_config language_yaml lsp plugin_manager restoretabs search_ui select_colorscheme selectionhighlight settings snippets tab_switcher
     fi
   fi
 }
