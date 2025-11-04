61ab7c8e92330be16c9bbe73f11fd7f391b8a42d
Iulian Donici <iuliandonici@gmail.com>
2025-11-04 19:54:03 +0100

    Updated.

--- functions/f_add_repo_brave_browser.sh
+++ functions/f_add_repo_brave_browser.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 function f_add_repo_brave_browser() {
+    source functions/f_check_networks.sh
     echo "- Currently adding the Brave browser repo using $(f_get_distro_packager):"
-    if [[ "$(f_get_distro_packager)" == "dnf" ]]; then
+    if [[ $(f_get_distro_packager) == "apk" ]]; then
+        if [[ $(f_check_networks) == "UP" ]]; then
+            echo "- but Brave browser isn't available on $(f_get_distro_id) using the $(f_get_distro_packager) package so instead we're using flatpak;"
+            if [[ "$EUID" -ne 0 ]]; then 
+                sudo $(f_get_distro_packager) add flatpak
+                sudo $(f_get_distro_packager) add discover-backend-flatpak
+                flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+                flatpak install flathub com.brave.Browser
+            else
+                $(f_get_distro_packager) add flatpak
+                $(f_get_distro_packager) add discover-backend-flatpak
+                flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+                flatpak install flathub com.brave.Browser
+            fi
+        else
+            echo "- but we can't install Brave browser because the networks are down;"
+        fi
+    elif [[ "$(f_get_distro_packager)" == "dnf" ]]; then
          if [[ "$EUID" -ne 0 ]]; then        
             sudo $(f_get_distro_packager) install -y dnf-plugins-core
             sudo $(f_get_distro_packager) config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
@@ -23,24 +41,28 @@ function f_add_repo_brave_browser() {
             rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
         fi
     elif [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
-        if [[ "$EUID" -ne 0 ]]; then 
-            sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-# Setting a variable for getting the machine's architecture
-            architecture=$(uname -m)
-            if [[ $architecture=="x64" || $architecture=="x86_64" ]]; then
-                echo "deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+        if [[ $(f_check_networks) == "UP" ]]; then
+            if [[ "$EUID" -ne 0 ]]; then 
+                sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+                # Setting a variable for getting the machine's architecture
+                architecture=$(uname -m)
+                if [[ $architecture=="x64" || $architecture=="x86_64" ]]; then
+                    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+                else
+                    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+                fi            
             else
-                echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-            fi            
-        else
-            curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-# Setting a variable for getting the machine's architecture
-            architecture=$(uname -m)
-            if [[ $architecture=="x64" || $architecture=="x86_64" ]]; then
-                echo "deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|tee /etc/apt/sources.list.d/brave-browser-release.list
-            else
-                echo "deb [signed-by=/usr/share/eyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|tee /etc/apt/sources.list.d/brave-browser-release.list
+                curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+                # Setting a variable for getting the machine's architecture
+                architecture=$(uname -m)
+                if [[ $architecture=="x64" || $architecture=="x86_64" ]]; then
+                    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|tee /etc/apt/sources.list.d/brave-browser-release.list
+                else
+                    echo "deb [signed-by=/usr/share/eyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|tee /etc/apt/sources.list.d/brave-browser-release.list
+                fi
             fi
+        else
+            echo "- but we can't install Brave browser because the networks are down;"
         fi
     fi
 }
