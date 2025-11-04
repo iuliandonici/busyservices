20cdd2c0177ea477ac9d7791c8bb6a3c794fe120
Iulian Donici <iuliandonici@gmail.com>
2025-11-04 21:10:55 +0100

    Updated.

--- functions/f_add_repo_flathub.sh
+++ functions/f_add_repo_flathub.sh
@@ -2,23 +2,41 @@
 function f_add_repo_flathub() {
   # Installing using the official documentation
   source functions/f_check_networks.sh
-  echo "- currently installing Flatkpak;"
+  # A variable to see what environment is currently installed
+  var_f_add_repo_flathub_desktop= printf 'Desktop: %s' "$XDG_CURRENT_DESKTOP" "$GDMSESSION"\
   if [[ $(f_get_distro_packager) == "apk" ]]; then
     if [[ $(f_check_networks) == "UP" ]]; then
       if [[ "$EUID" -ne 0 ]]; then
         # Install the package
         doas apk add flatpak
-        doas apk add discover-backend-flatpak
         # Install the Software Flatpak plugin for KDE
-        doas apk add discover-backend-flatpak
+        echo "- and we're using $var_f_add_repo_flathub_desktop;"
+        echo "- currently installing the Software Flatpak plugin;"
+          # if its' KDE
+          if [[ $var_f_add_repo_flathub_desktop == "KDE" ]]; then
+            doas apk add discover-backend-flatpak
+          # or Gnome
+          elif [[ $var_f_add_repo_flathub_desktop == "Gnome" ]]; then
+            doas apk add gnome-software-plugin-flatpak
+          fi
         # Add the Flathub repository
+        echo "- currently adding the Flathub repository;"
         flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
       else
+        # Install the package
         apk add flatpak
-        apk add discover-backend-flatpak
         # Install the Software Flatpak plugin for KDE
-        apk add discover-backend-flatpak
+        echo "- and we're using $var_f_add_repo_flathub_desktop;"
+        echo "- currently installing the Software Flatpak plugin;"
+          # if its' KDE
+          if [[ $var_f_add_repo_flathub_desktop == "KDE" ]]; then
+            apk add discover-backend-flatpak
+          # or Gnome
+          elif [[ $var_f_add_repo_flathub_desktop == "Gnome" ]]; then
+            apk add gnome-software-plugin-flatpak
+          fi
         # Add the Flathub repository
+        echo "- currently adding the Flathub repository;"
         flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
       fi
     else
