8a32dc86ca028be9cdebe681b129c3b0a3115118
Iulian Donici <iuliandonici@gmail.com>
2025-11-12 18:22:12 +0100

    Updated.

--- show.sh
+++ show.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 function f_install_gitea() {
-  var_install_gitea_software_array=("gitea" "mariadb" "mariadb-client")
-  source functions/f_update_software.sh
-  source functions/f_get_distro_packager.sh
-  source functions/f_check_networks.sh
-  echo " - installing Gitea;"
-  if [[ $(f_get_distro_packager) == "apk" ]]; then
-    if [[ $(f_check_networks) == "UP" ]]; then
-      echo "- and here's a list of base software that will be installed using $(f_get_distro_packager):"
-      for i in "${!var_install_gitea_software_array[@]}"
-      do
-          echo " $i ${var_install_gitea_software_array[$i]}"
-      done
-      f_update_software
-      for i in "${!var_install_kde_software_array[@]}"
-      do
-          echo "- and currently installing: $i ${var_install_gitea_software_array[$i]}"
-          if [[ "$EUID" -ne 0 ]]; then 
-              doas $(f_get_distro_packager) add ${var_install_gitea_software_array[$i]}  
-          else
-              $(f_get_distro_packager) add ${var_install_gitea_software_array[$i]}  
-          fi
-      done
-    else
-          echo "- but can't install them because the networks are down;"
+  var_f_config_network_wired_interfaces_status=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $9}' | sed -e 's/://g')
+  if [[ $var_f_config_network_wired_interfaces_status == "bridge0" ]]; then
+    var_f_config_network_wired_interfaces_status=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $11}' | sed -e 's/://g')
+  fi  
+  var_f_config_network_wireless_interfaces=$(ip a | grep -E "wl.*:" | awk '{print $2}' | sed -e 's/://g')
+  var_f_config_network_wireless_interfaces_status=$(ip a | grep -E "wl.*:" | awk '{print $9}' | sed -e 's/://g')
+  if ([[ -z $var_f_config_kvm_network_wired_interfaces ]] && [[ ! -z $var_f_config_network_wireless_interfaces ]]) || ([[ ! -z $var_f_config_kvm_network_wired_interfaces ]] && [[ -z $var_f_config_network_wireless_interfaces ]]); then
+    if ([[ $var_f_config_network_wired_interfaces_status == "UP" ]] || [[ $var_f_config_network_wireless_interfaces_status == "UP" ]]); then
+      echo "UP"
+    else 
+      echo "DOWN"
     fi
-  else 
-    echo "- but Gitea wasn't tested on this distro;"
   fi
 }
 f_install_gitea
