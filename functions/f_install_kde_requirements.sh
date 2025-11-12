24f7103e1d0d5d91cf139476a906b68005748970
Iulian Donici <iuliandonici@gmail.com>
2025-11-12 18:02:08 +0100

    Added USB support..

--- functions/f_install_kde_requirements.sh
+++ functions/f_install_kde_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-var_install_kde_software_array=("plasma elogind" "polkit-elogind" "kde-applications-admin" "kde-applications-network" "kde-applications-utils" "kde-applications-base" "font-terminus" "font-inconsolata" "font-dejavu" "font-noto" "font-noto-cjk" "font-awesome" "font-noto-extra" "font-liberation" "python3" "networkmanager" "networkmanager-tui" "networkmanager-cli" "plasma-nm" "network-manager-applet" "networkmanager-wifi")
+var_install_kde_software_array=("plasma elogind" "polkit-elogind" "kde-applications-admin" "kde-applications-network" "kde-applications-utils" "kde-applications-base" "font-terminus" "font-inconsolata" "font-dejavu" "font-noto" "font-noto-cjk" "font-awesome" "font-noto-extra" "font-liberation" "python3" "networkmanager" "networkmanager-tui" "networkmanager-cli" "plasma-nm" "network-manager-applet" "networkmanager-wifi" "virt-manager")
 function f_install_kde_requirements() {
     source functions/f_update_software.sh
     source functions/f_get_distro_packager.sh
