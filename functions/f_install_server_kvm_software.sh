867cd4ad82674433a2597043d4369ef38f123d6d
Iulian Donici <iuliandonici@gmail.com>
2025-11-12 17:14:18 +0100

    Updated.

--- functions/f_install_server_kvm_software.sh
+++ functions/f_install_server_kvm_software.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 var_install_server_kvm_software_array=("virt-install" "dbus" "polkit" "virt-manager" "font-terminus" "qemu" "qemu-kvm" "qemu-system-x86_64" "qemu-img" "qemu-guest-agent" "qemu-tools" "libvirt" "libvirt-daemon" "libvirt-qemu" "libvirt-daemon-system" "libvirt-clients" "bridge-utils" "ifupdown")
-# qemu qemu-system-x86_64 qemu-img
 function f_install_server_kvm_software() {
     source functions/f_update_software.sh
     source functions/f_config_kvm_sshd.sh
