810778aceb6ffb8845c2b881fb0f8afd2b158822
Iulian Donici <iuliandonici@gmail.com>
2025-11-14 17:34:25 +0100

    Updated.

--- show.sh
+++ show.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function f_install_server_dev_software() {
-    source functions/f_install_nginx_requirements
+    source functions/f_install_nginx_requirements.sh
     f_install_nginx_requirements
 }
 echo "testing"
