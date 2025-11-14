902b494c34f2ade18939e0ccc2a6c1264a7427c4
Iulian Donici <iuliandonici@gmail.com>
2025-11-14 12:47:31 +0100

    Iterating possible network connection names..

--- show.sh
+++ show.sh
@@ -1,21 +1,74 @@
 #!/bin/bash
-function f_install_nginx() {
-  source functions/f_update_software.sh
-  source functions/f_get_distro_packager.sh
-  source functions/f_check_networks.sh
-  var_install_nginx_software_array=("nginx")
-  if [[ $(f_get_distro_packager) == "apt" ]]; then
-    echo "- and currently installing nginx;"
-    if [[ $(f_check_networks) == "UP" ]]; then
-      if [[ "$EUID" -ne 0 ]]; then 
-          sudo $(f_get_distro_packager) install -y ${var_install_nginx_software_array[$i]}
-      else
-          $(f_get_distro_packager) install -y ${var_install_nginx_software_array[$i]}  
-      fi
+function f_config_nginx() {
+    # Verify if Nginx has been installed
+    if [[ -f /usr/sbin/nginx ]]; then
+        echo "- Nginx is installed, now we'll config it."
+        if [[ "$EUID" -ne 0 ]]; then 
+            sudo rm -rf /var/www/html/
+            sudo rm -rf /etc/nginx/sites-enabled/default
+            sudo rm -rf /etc/nginx/sites-available/default
+            case $(hostname) in
+                busydev)
+                    echo "This is $(hostname)."
+                    sudo rm -rf /etc/nginx/sites-enabled/busydev
+                    sudo rm -rf /etc/nginx/sites-available/busydev
+                    sudo rm -rf /var/www/busydev/
+                    sudo mkdir /var/www/busydev/
+                    sudo cp -r functions/f_config_nginx_dev.html index.html
+                    sudo mv index.html /var/www/busydev/
+                    sudo cp -r functions/f_config_nginx_dev busydev
+                    sudo mv busydev /etc/nginx/sites-available/
+                    sudo ln -s /etc/nginx/sites-available/busydev /etc/nginx/sites-enabled/busydev
+                ;;
+                busycenter)
+                    echo "This is $(hostname)."
+                    sudo rm -rf /etc/nginx/sites-enabled/busyprod
+                    sudo rm -rf /etc/nginx/sites-available/busyprod
+                    sudo rm -rf /var/www/busyprod/
+                    sudo mkdir /var/www/busyprod/
+                    sudo cp -r functions/f_config_nginx_prod.html index.html
+                    sudo mv index.html /var/www/busyprod/
+                    sudo cp -r functions/f_config_nginx_prod busyprod
+                    sudo mv busyprod /etc/nginx/sites-available/
+                    sudo ln -s /etc/nginx/sites-available/busyprod /etc/nginx/sites-enabled/busyprod
+                ;;
+            esac
+            sudo service nginx restart
+        else
+            rm -rf /var/www/html/
+            rm -rf /etc/nginx/sites-enabled/default
+            rm -rf /etc/nginx/sites-available/default
+            case $(hostname) in
+                busydev)
+                    echo "This is $(hostname)."
+                    rm -rf /etc/nginx/sites-enabled/busydev
+                    rm -rf /etc/nginx/sites-available/busydev
+                    rm -rf /var/www/busydev/
+                    mkdir /var/www/busydev/
+                    cp -r functions/f_config_nginx_dev.html index.html
+                    mv index.html /var/www/busydev/
+                    cp -r functions/f_config_nginx_dev busydev
+                    mv busydev /etc/nginx/sites-available/
+                    ln -s /etc/nginx/sites-available/busydev /etc/nginx/sites-enabled/busydev
+                ;;
+                busycenter)
+                    echo "This is $(hostname)."
+                    rm -rf /etc/nginx/sites-enabled/busyprod
+                    rm -rf /etc/nginx/sites-available/busyprod
+                    rm -rf /var/www/busyprod/
+                    mkdir /var/www/busyprod/
+                    cp -r functions/f_config_nginx_prod.html index.html
+                    mv index.html /var/www/busyprod/
+                    cp -r functions/f_config_nginx_prod busyprod
+                    mv busyprod /etc/nginx/sites-available/
+                    ln -s /etc/nginx/sites-available/busyprod /etc/nginx/sites-enabled/busyprod
+                ;;
+            esac
+            service nginx restart
+        fi
     else
-      echo "- but can't install it because the networks are down;"
+        echo "- Nginx isn't installed:"
+        ls -alh /usr/sbin/ | grep "nginx"
     fi
-  fi
 }
-f_install_nginx
-
+f_config_nginx
