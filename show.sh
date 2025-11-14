b859c9c695813feab308f1ac4488c53c9c8d471b
Iulian Donici <iuliandonici@gmail.com>
2025-11-14 12:29:25 +0100

    Introducing nginx function.

--- show.sh
+++ show.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-source functions/f_install_gitea.sh
-f_install_giteak
+function f_install_nginx() {
+  source functions/f_update_software.sh
+  source functions/f_get_distro_packager.sh
+  source functions/f_check_networks.sh
+  var_install_nginx_software_array=("nginx")
+  if [[ $(f_get_distro_packager) == "apt" ]]; then
+    echo "- and currently installing nginx;"
+    if [[ $(f_check_networks) == "UP" ]]; then
+      if [[ "$EUID" -ne 0 ]]; then 
+          sudo $(f_get_distro_packager) install -y ${var_install_nginx_software_array[$i]}
+      else
+          $(f_get_distro_packager) install -y ${var_install_nginx_software_array[$i]}  
+      fi
+    else
+      echo "- but can't install it because the networks are down;"
+    fi
+  fi
+}
+f_install_nginx
+
