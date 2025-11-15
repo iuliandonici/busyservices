749c7ac6cee8c272efa6754376728c99d6bcfa0b
Iulian Donici <iuliandonici@gmail.com>
2025-11-15 07:25:42 +0100

    Updated dev software.

--- show.sh
+++ show.sh
@@ -3,5 +3,4 @@ function f_install_server_dev_software() {
     source functions/f_install_nginx_requirements.sh
     f_install_nginx_requirements
 }
-echo "testing now:"
 f_install_server_dev_software
