526cc10fea2c8938c0a00a471070dbaf5eff7f65
Iulian Donici <iuliandonici@gmail.com>
2025-11-23 13:50:34 +0100

    Updated.

--- functions/f_config_transmission_htpasswd.sh
+++ functions/f_config_transmission_htpasswd.sh
@@ -1,5 +1,5 @@
 function f_config_transmission_htpasswd () {
-  echo "- trying to secure the service using htpasswd;"
+  echo "- and currently securing the service using htpasswd;"
   if [[ -f /usr/share/transmission/public_html/.htpasswd ]]; then
     echo " - but the service has already been secured;"
   else
