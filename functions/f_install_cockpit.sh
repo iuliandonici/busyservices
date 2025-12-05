ce0440ef164f1713aa5dee79ff311f756098f2a3
Iulian Donici <iuliandonici@gmail.com>
2025-12-05 18:59:26 +0100

    Updated.

---
+++ functions/f_install_cockpit.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+var_install_cockpit_software_array_debian=("cockpit" "cockpit-machines" "cockpit-storaged" "cockpit-networkmanager" "cockpit-packagekit" "cockpit-ostree" "cockpit-machines" "cockpit-podman
+" "cockpit-selinux" "cockpit-kdump" "cockpit-sosreport" "cockpit-files")
+function f_install_cockpit() {
+    source functions/f_update_software.sh
+    echo " - here's a list of extra software that will be installed using $(f_get_distro_packager):"
+    for i in "${!var_install_cockpit_software_array_debian[@]}"
+    do
+        echo " $i ${var_install_cockpit_software_array_debian[$i]}"
+    done
+    if [[ $(f_get_distro_packager) == "apk" ]]; then
+        for i in "${!var_install_cockpit_software_array_debian[@]}"
+        do
+            echo "- Currently installing: $i ${var_install_cockpit_software_array_debian[$i]}"
+            if [[ "$EUID" -ne 0 ]]; then 
+                doas $(f_get_distro_packager) add ${var_install_cockpit_software_array_debian[$i]}  
+            else
+                $(f_get_distro_packager) add ${var_install_cockpit_software_array_debian[$i]}  
+            fi
+        done     
+    elif [[ $(f_get_distro_packager) == "dnf" || $(f_get_distro_packager) == "zypper" ]]; then
+        for i in "${!var_install_cockpit_software_array_debian[@]}"
+        do
+            echo "- Currently installing: $i ${var_install_cockpit_software_array_debian[$i]}"
+            if [[ "$EUID" -ne 0 ]]; then 
+                sudo $(f_get_distro_packager) install -y ${var_install_cockpit_software_array_debian[$i]}  
+            else
+                $(f_get_distro_packager) install -y ${var_install_cockpit_software_array_debian[$i]}  
+            fi
+        done        
+    else
+        . /etc/os-release
+        for i in "${!var_install_cockpit_software_array_debian[@]}"
+        do
+            echo "- Currently installing: $i ${var_install_cockpit_software_array_debian[$i]}"
+            if [[ "$EUID" -ne 0 ]]; then 
+                sudo $(f_get_distro_packager) install -y -t ${VERSION_CODENAME}-backports ${var_install_cockpit_software_array_debian[$i]}  
+            else
+                $(f_get_distro_packager) install -y ${var_install_cockpit_software_array_debian[$i]}  
+            fi
+        done
+    fi
+    f_update_software
+}
