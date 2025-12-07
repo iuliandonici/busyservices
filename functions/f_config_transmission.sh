39cfb1b58ce144ad70b366220b7da0eeeffcef0a
Iulian Donici <iuliandonici@gmail.com>
2025-12-07 17:51:11 +0100

    Updated.

--- functions/f_config_transmission.sh
+++ functions/f_config_transmission.sh
@@ -14,8 +14,7 @@ function f_config_transmission() {
             sudo chmod 777 ~/
             sudo chown debian-transmission:debian-transmission /var/lib/transmission-daemon/info/settings.json
             pwd
-                source functions/f_config_transmission_htpasswd.sh
-
+            
             sudo f_config_transmission_htpasswd
             sudo systemctl start transmission-daemon.service
             sudo systemctl daemon-reload
