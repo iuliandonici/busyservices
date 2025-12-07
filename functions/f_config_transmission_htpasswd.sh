d81f02549a7e6e2db8a11a257a4f144db7f3f69d
Iulian Donici <iuliandonici@gmail.com>
2025-12-07 17:31:49 +0100

    Updated.

--- functions/f_config_transmission_htpasswd.sh
+++ functions/f_config_transmission_htpasswd.sh
@@ -4,6 +4,10 @@ function f_config_transmission_htpasswd () {
   if [[ -f /usr/share/transmission/public_html/.htpasswd ]]; then
     echo " - but the service has already been secured;"
   else
-    htpasswd -c /usr/share/transmission/public_html/.htpasswd $USER
+    if [[ "$EUID" -ne 0 ]]; then
+      sudo htpasswd -c /usr/share/transmission/public_html/.htpasswd $USER
+    else
+      htpasswd -c /usr/share/transmission/public_html/.htpasswd $USER
+    fi
   fi
 }
