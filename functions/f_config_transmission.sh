01c734f8abaac58a9dfaca7e37816f6399e7ab55
Iulian Donici <iuliandonici@gmail.com>
2025-12-07 15:37:32 +0100

    Fixed the .service location.

--- functions/f_config_transmission.sh
+++ functions/f_config_transmission.sh
@@ -6,7 +6,7 @@ function f_config_transmission() {
         echo "- Transmission is installed, now we'll config it."
         if [[ "$EUID" -ne 0 ]]; then 
             sudo systemctl stop transmission-daemon.service
-            sudo sed -i -e 's/Type=notify/Type=simple/g' /etc/systemd/system/transmission-daemon.service
+            sudo sed -i -e 's/Type=notify/Type=simple/g' /lib/systemd/system/transmission-daemon.service
             sudo cp -r functions/f_config_transmission.json settings.json
             sudo mv settings.json /var/lib/transmission-daemon/info/
             sudo usermod -a -G debian-transmission $USER
@@ -17,7 +17,7 @@ function f_config_transmission() {
             sudo systemctl daemon-reload
         else
             systemctl stop transmission-daemon.service
-            sed -i -e 's/Type=notify/Type=simple/g' /etc/systemd/system/transmission-daemon.service
+            sed -i -e 's/Type=notify/Type=simple/g' /lib/systemd/system/transmission-daemon.service
             cp -r functions/f_config_transmission.json settings.json 
             mv settings.json /var/lib/transmission-daemon/info/
             usermod -a -G debian-transmission $USER
