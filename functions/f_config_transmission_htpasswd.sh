89a8971c0a6c87e98f20f0c04699f4bfd327f384
Iulian Donici <iuliandonici@gmail.com>
2025-12-07 17:28:18 +0100

    Updated.

--- functions/f_config_transmission_htpasswd.sh
+++ functions/f_config_transmission_htpasswd.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -i
 function f_config_transmission_htpasswd () {
   echo " - and currently securing the service using htpasswd;"
   if [[ -f /usr/share/transmission/public_html/.htpasswd ]]; then
