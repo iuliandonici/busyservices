ac70cc503f0dce2f7dca21694028b19fdc573e23
Iulian Donici <iuliandonici@gmail.com>
2025-12-16 20:09:52 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if sudo grep -wq "renderer" "/etc/netplan/50-cloud-init.yaml"; then
-    echo "- but NetworkManager as renderer is already being used;"
-else
-    echo -e "\t renderer: NetworkManager";
-fi
+function f_add_network_manager() {
+  if sudo grep -wq "renderer" "/etc/netplan/50-cloud-init.yaml"; then
+      echo "- but NetworkManager as renderer is already being used;"
+  else
+      sudo echo -e "\t renderer: NetworkManager" >> /etc/netplan/50-cloud-init.yaml
+  fi
+}
