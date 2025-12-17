7c546917f2617dfd66d6ed6ffd57f1ce9db53795
Iulian Donici <iuliandonici@gmail.com>
2025-12-17 18:04:10 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -2,16 +2,21 @@
 # Installing 
 function f_install_acf() {
   echo "- Currently setting up ACF (Alpine Configuration Framework):"
-  if [ -d /usr/share/acf ]; then
-    echo "- but ACF is already installed;"
-  else
-    if [[ "$EUID" -ne 0 ]]; then 
-      doas setup-acf
-      doas acfpasswd -s root
+  if [[ $(f_get_distro_packager) == "apk" ]]; then
+
+    if [ -d /usr/share/acf ]; then
+      echo "- but ACF is already installed;"
     else
-      setup-acf
-      acfpasswd -s root
+      if [[ "$EUID" -ne 0 ]]; then 
+        doas setup-acf
+        doas acfpasswd -s root
+      else
+        setup-acf
+        acfpasswd -s root
+      fi
     fi
+  else
+    echo "- but this function hasn't been tested on this os;"
   fi
 }
 f_install_acf
