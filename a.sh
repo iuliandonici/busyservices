f16310cfa76dd5b34ef4d2a0b4358df0f06cc2b6
Iulian Donici <iuliandonici@gmail.com>
2025-12-17 17:52:16 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
-var_add_netplan_network_manager_spacer="    "
-function f_add_netplan_network_manager() {
-  source functions/f_get_distro_packager.sh
-  echo "- Currently configuring netplan:"
-  if [[ $(f_get_distro_packager) == "apt" || $(f_get_distro_packager) == "apt-get" ]]; then
-    if [[ "$EUID" -ne 0 ]]; then 
-      if sudo grep -wq "renderer" "/etc/netplan/50-cloud-init.yaml"; then
-        echo "- but NetworkManager as renderer is already being used;"
-      else
-        sudo chmod 0600 /etc/netplan/*.yaml
-        echo "${var_add_netplan_network_manager_spacer}renderer: NetworkManager" | sudo tee -a /etc/netplan/50-cloud-init.yaml
-        sudo netplan apply
-      fi
-    else
-      if grep -wq "renderer" "/etc/netplan/50-cloud-init.yaml"; then
-        echo "- but NetworkManager as renderer is already being used;"
-      else
-        chmod 600 /etc/netplan/50-cloud-init.yaml
-        echo -e "\trenderer: NetworkManager" >> /etc/netplan/50-cloud-init.yaml
-        netplan apply
-      fi
-    fi
+# Installing 
+function f_install_acf() {
+  echo "- Currently setting up ACF (Alpine Configuration Framework):"
+  if [ -d /usr/share/acf ]; then
+    echo "- but ACF is already installed;"
   else
-    echo "- but this function hasn't been tested on this os;"
+    if [[ "$EUID" -ne 0 ]]; then 
+      doas setup-acf
+      doas acfpasswd -s root
+    else
+      setup-acf
+      acfpasswd -s root
+    fi
   fi
 }
-f_add_netplan_network_manager
+f_install_acf
