175ff4f4c0c1c174e7a267d2e54c29690ef40d76
Iulian Donici <iuliandonici@gmail.com>
2025-12-31 12:08:08 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 function f_install_dockge() {
-    # Create directories that store your stacks and store Dockge's stack
-    mkdir -p /opt/stacks /opt/dockge
-    cd /opt/dockge
-    # Download your compose.yaml
-    curl "https://dockge.kuma.pet/compose.yaml?port=5001&stacksPath=%2Fopt%2Fstacks" --output compose.yaml
-
-    # Start the Server
-    # docker compose up -d
-
-    # If you are using docker-compose V1 or Podman
-    docker-compose up -d
+    echo "- installing Dockge:"
+    if [[ $(f_get_distro_packager) == "apk" ]]; then
+        # Create directories that store your stacks and store Dockge's stack
+        doas mkdir -p /opt/busystacks /opt/busycontainers/bc-dockge
+        # Copy a default Dockge config
+        doas cp -r functions/f_install_dockge.yaml /opt/busycontainers/bc-dockge/compose.yaml
+        cd /opt/busycontainers/bc-dockge/
+        # Start the Server
+        # docker compose up -d
+        # If you are using docker-compose V1 or Podman
+        docker-compose up -d
+    elif [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
+        # Create directories that store your stacks and store Dockge's stack
+        sudo mkdir -p /opt/busystacks /opt/busycontainers/bc-dockge
+        # Copy a default Dockge config
+        sudo cp -r functions/f_install_dockge.yaml /opt/busycontainers/bc-dockge/compose.yaml
+        cd /opt/busycontainers/bc-dockge/
+        # Start the Server
+        # docker compose up -d
+        # If you are using docker-compose V1 or Podman
+        docker-compose up -d
+    fi
     }
 f_install_dockge
