b9069ecf95f4ea9830f79aa63631eec1297548c8
Iulian Donici <iuliandonici@gmail.com>
2026-01-01 11:52:23 +0100

    Updated.

--- functions/f_install_dockge.sh
+++ functions/f_install_dockge.sh
@@ -4,24 +4,27 @@ function f_install_dockge() {
     echo "- installing Dockge:"
     if [[ $(f_get_distro_packager) == "apk" ]]; then
         if [[ "$EUID" -ne 0 ]]; then 
+            doas rc-service docker stop
             doas docker container rm -f busydockge-container
             # Create directories that store your stacks and store Dockge's stack
             doas mkdir -p ~/busycontainers/busydockge-container/
             # Copy a default Dockge config
             doas cp -r functions/f_install_dockge.yaml ~/busycontainers/busydockge-container/compose.yaml
-            doas rc-service docker restart
+            doas rc-service docker start
         else
+            rc-service docker stop
             docker container rm -f busydockge-container
             # Create directories that store your stacks and store Dockge's stack
             mkdir -p ~/busycontainers/busydockge-container/
             # Copy a default Dockge config
             cp -r functions/f_install_dockge.yaml ~/busycontainers/busydockge-container/compose.yaml
-            doas rc-service docker restart
+            rc-service docker start
         fi
         cd ~/busycontainers/busydockge-container/
         # Start the Server
         # docker compose up -d
         # If you are using docker-compose V1 or Podman
+        echo "- composing:"
         docker-compose up -d
     elif [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
         # Create directories that store your stacks and store Dockge's stack
