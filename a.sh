4978262665e054e14c36f406634dffa04fa36a8f
Iulian Donici <iuliandonici@gmail.com>
2026-01-01 12:49:07 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,37 +1,16 @@
 #!/bin/bash
-function f_install_dockge() {
-    source functions/f_get_distro_packager.sh
-    echo "- installing Dockge:"
-    if [[ $(f_get_distro_packager) == "apk" ]]; then
-        if [[ "$EUID" -ne 0 ]]; then 
-            doas docker container rm -f busydockge-container
-            # Create directories that store your stacks and store Dockge's stack
-            doas mkdir -p ~/busycontainers/busydockge-container/
-            # Copy a default Dockge config
-            doas cp -r functions/f_install_dockge.yaml ~/busycontainers/busydockge-container/compose.yaml
-        else
-            docker container rm -f busydockge-container
-            # Create directories that store your stacks and store Dockge's stack
-            mkdir -p ~/busycontainers/busydockge-container/
-            # Copy a default Dockge config
-            cp -r functions/f_install_dockge.yaml ~/busycontainers/busydockge-container/compose.yaml
-        fi
-        cd ~/busycontainers/busydockge-container/
-        # Start the Server
-        # docker compose up -d
-        # If you are using docker-compose V1 or Podman
-        echo "- composing:"
-        docker-compose up -d
-    elif [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
-        # Create directories that store your stacks and store Dockge's stack
-        sudo mkdir -p /opt/busystacks /opt/busycontainers/bc-dockge
-        # Copy a default Dockge config
-        sudo cp -r functions/f_install_dockge.yaml /opt/busycontainers/bc-dockge/compose.yaml
-        cd /opt/busycontainers/bc-dockge/
-        # Start the Server
-        # docker compose up -d
-        # If you are using docker-compose V1 or Podman
-        docker-compose up -d
-    fi
+function f_check_networks() {
+  var_f_config_network_wired_interfaces=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $2}' | sed -e 's/://g')
+  var_f_config_network_wired_interfaces_status=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $9}' | sed -e 's/://g')
+  if [[ $var_f_config_network_wired_interfaces_status == "bridge0" ]]; then
+    var_f_config_network_wired_interfaces_status=$(ip a | grep -E "en.*:|es.*:|eth[0-99]:" | awk '{print $11}' | sed -e 's/://g')
+  fi
+  var_f_config_network_wireless_interfaces=$(ip a | grep -E "wl.*:" | awk '{print $2}' | sed -e 's/://g')
+  var_f_config_network_wireless_interfaces_status=$(ip a | grep -E "wl.*:" | awk '{print $9}' | sed -e 's/://g')
+  if ([[ ! -z $var_f_config_network_wired_interfaces ]] && [[ $var_f_config_network_wired_interfaces_status == "UP" ]]) || ([[ ! -z $var_f_config_network_wireless_interfaces ]] && [[ $var_f_config_network_wireless_interfaces_status == "UP" ]]) ; then
+    echo "UP"
+  else
+    echo "DOWN"
+  fi
 }
-f_install_dockge
+f_check_networks
