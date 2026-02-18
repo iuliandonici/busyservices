10fab63d09a79f9724c5dc94db686c1fb2c136f9
Iulian Donici <iuliandonici@gmail.com>
2026-02-18 15:13:04 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,156 +1,74 @@
 #!/bin/bash
-var_install_jellyfin_software_array=("jellyfin" "jellyfin-web")
-function f_install_jellyfin() {
+var_install_transmission_requirements=("transmission-cli" "transmission-common" "transmission-daemon")
+var_install_transmission_requirements_alpine=("transmission-cli" "transmission-daemon")
+var_install_transmission_requirements_debian=("transmission-cli" "transmission-common" "transmission-daemon")
+function f_install_transmission_requirements() {
     source functions/f_update_software.sh
-    source functions/f_get_distro_packager.sh
-    if [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
-        echo " - currently adding the repo and then installing Jellyfin using $(f_get_distro_packager):"
-        if [[ "$EUID" -ne 0 ]]; then 
-            # Setting a variable for getting the machine's architecture
-            architecture=$(uname -m)
-            if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
-                sudo mkdir /etc/apt/keyrings
-                DISTRO="$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release )"
-                CODENAME="$( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release )"
-                curl -fsSL https://repo.jellyfin.org/${DISTRO}/jellyfin_team.gpg.key | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/jellyfin.gpg
-                cat <<EOF | sudo tee /etc/apt/sources.list.d/jellyfin.sources
-Types: deb
-URIs: https://repo.jellyfin.org/${DISTRO}
-Suites: ${CODENAME}
-Components: main
-Architectures: $( dpkg --print-architecture )
-Signed-By: /etc/apt/keyrings/jellyfin.gpg
-EOF
-                echo "- and here's a list of software needed using $(f_get_distro_packager):"
-                for i in "${!var_install_jellyfin_software_array[@]}"
-                do
-                    echo " $i ${var_install_jellyfin_software_array[$i]}"
-                done
-                for i in "${!var_install_jellyfin_software_array[@]}"
-                do
-                    echo " - currently installing: $i ${var_install_jellyfin_software_array[$i]}"
-                    if [[ "$EUID" -ne 0 ]]; then 
-                        sudo $(f_get_distro_packager) install -y ${var_install_jellyfin_software_array[$i]}  
-                    else
-                        $(f_get_distro_packager) install -y ${var_install_jellyfin_software_array[$i]}  
-                    fi
-                done
+    source functions/f_config_transmission.sh
+    f_update_software
+
+    if [[ $(f_get_distro_packager) == "apk" ]]; then
+        echo "- List of base software that will be installed using $(f_get_distro_packager):"
+        for i in "${!var_install_transmission_requirements_alpine[@]}"
+        do
+            echo " $i ${var_install_transmission_requirements_alpine[$i]}"
+        done
+        for i in "${!var_install_transmission_requirements_alpine[@]}"
+        do
+            echo " - and currently installing: $i ${var_install_transmission_requirements_alpine[$i]}"
+            if [[ "$EUID" -ne 0 ]]; then 
+                doas $(f_get_distro_packager) add ${var_install_transmission_requirements_alpine[$i]}  
             else
-                echo "- but here is no version of Jellyfin for this architecture ($architecture);"
-            fi            
-        else
-# Setting a variable for getting the machine's architecture
-            architecture=$(uname -m)
-            if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
-                mkdir /etc/apt/keyrings
-                DISTRO="$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release )"
-                CODENAME="$( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release )"
-                curl -fsSL https://repo.jellyfin.org/${DISTRO}/jellyfin_team.gpg.key --no-check-certificate | gpg --batch --yes --dearmor -o /etc/apt/keyrings/jellyfin.gpg
-                cat <<EOF | tee /etc/apt/sources.list.d/jellyfin.sources
-Types: deb
-URIs: https://repo.jellyfin.org/${DISTRO}
-Suites: ${CODENAME}
-Components: main
-Architectures: $( dpkg --print-architecture )
-Signed-By: /etc/apt/keyrings/jellyfin.gpg
-EOF
-                echo "- and here's a list of software needed using $(f_get_distro_packager):"
-                for i in "${!var_install_jellyfin_software_array[@]}"
-                do
-                    echo " $i ${var_install_jellyfin_software_array[$i]}"
-                done
-                for i in "${!var_install_jellyfin_software_array[@]}"
-                do
-                    echo " - currently installing: $i ${var_install_jellyfin_software_array[$i]}"
-                    if [[ "$EUID" -ne 0 ]]; then 
-                        $(f_get_distro_packager) install -y ${var_install_jellyfin_software_array[$i]}  
-                    else
-                        $(f_get_distro_packager) install -y ${var_install_jellyfin_software_array[$i]}  
-                    fi
-                done
+                $(f_get_distro_packager) add ${var_install_transmission_requirements_alpine[$i]}  
+            fi
+        done     
+    elif [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
+        echo "- List of base software that will be installed using $(f_get_distro_packager):"
+        for i in "${!var_install_transmission_requirements_debian[@]}"
+        do
+            echo " $i ${var_install_transmission_requirements_debian[$i]}"
+        done
+        for i in "${!var_install_transmission_requirements_debian[@]}"
+        do
+            echo " - and currently installing: $i ${var_install_transmission_requirements_debian[$i]}"
+            if [[ "$EUID" -ne 0 ]]; then 
+                sudo $(f_get_distro_packager) install -y ${var_install_transmission_requirements_debian[$i]}  
             else
-                echo "- but here is no version of Jellyfin for this architecture ($architecture);"
-            fi            
-        fi
-    elif [[ "$(f_get_distro_packager)" == "apk" ]]; then
-        echo " - currently installing Jellyfin using $(f_get_distro_packager):"
-        if [[ "$EUID" -ne 0 ]]; then 
-            # Setting a variable for getting the machine's architecture
-            architecture=$(uname -m)
-            if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
-                # echo "https://repo.jellyfin.org/alpine/latest" | doas tee -a /etc/apk/repositories
-                # wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key --no-check-certificate | doas tee /etc/apk/keys/jellyfin_team.rsa.pub >/dev/null
-                echo "- and here's a list of software needed using $(f_get_distro_packager):"
-                for i in "${!var_install_jellyfin_software_array[@]}"
-                do
-                    echo " $i ${var_install_jellyfin_software_array[$i]}"
-                done
-                for i in "${!var_install_jellyfin_software_array[@]}"
-                do
-                    echo " - currently installing: $i ${var_install_jellyfin_software_array[$i]}"
-                    if [[ "$EUID" -ne 0 ]]; then 
-                        # f_update_software
-                        doas $(f_get_distro_packager) add ${var_install_jellyfin_software_array[$i]}
-                        # f_update_software
-                        doas rc-update add ${var_install_jellyfin_software_array[0]} boot
-                        # f_update_software
-                        doas sed -i 's/--nowebclient//' /etc/conf.d/jellyfin
-                        doas rc-service ${var_install_jellyfin_software_array[0]} restart
-                    else
-                        # f_update_software
-                        $(f_get_distro_packager) add ${var_install_jellyfin_software_array[$i]}
-                        # f_update_software
-                        rc-update add ${var_install_jellyfin_software_array[0]} boot
-                        # f_update_software
-                        sed -i 's/--nowebclient//' /etc/conf.d/jellyfin
-                        rc-service ${var_install_jellyfin_software_array[0]} restart
-                    fi
-                done
+                $(f_get_distro_packager) install -y ${var_install_transmission_requirements_debian[$i]}  
+            fi
+        done         
+    elif [[ $(f_get_distro_packager) == "dnf" || $(f_get_distro_packager) == "zypper" ]]; then
+        echo "- List of base software that will be installed using $(f_get_distro_packager):"
+        for i in "${!var_install_transmission_requirements[@]}"
+        do
+            echo " $i ${var_install_transmission_requirements[$i]}"
+        done
+        for i in "${!var_install_transmission_requirements[@]}"
+        do
+            echo " - and currently installing: $i ${var_install_transmission_requirements[$i]}"
+            if [[ "$EUID" -ne 0 ]]; then 
+                sudo $(f_get_distro_packager) install -y ${var_install_transmission_requirements[$i]}  
             else
-                echo "- but here is no version of Jellyfin for this architecture ($architecture);"
-            fi            
-        else
-            # Setting a variable for getting the machine's architecture
-            architecture=$(uname -m)
-            if [[ $architecture == "x64" || $architecture == "x86_64" ]]; then
-                # echo "https://repo.jellyfin.org/alpine/latest" | tee -a /etc/apk/repositories
-                # wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key --no-check-certificate | tee /etc/apk/keys/jellyfin_team.rsa.pub >/dev/null
-                echo "- and here's a list of software needed using $(f_get_distro_packager):"
-                for i in "${!var_install_jellyfin_software_array[@]}"
-                do
-                    echo " $i ${var_install_jellyfin_software_array[$i]}"
-                done
-                for i in "${!var_install_jellyfin_software_array[@]}"
-                do
-                    echo " - currently installing: $i ${var_install_jellyfin_software_array[$i]}"
-                    if [[ "$EUID" -ne 0 ]]; then 
-                        doas $(f_get_distro_packager) add ${var_install_jellyfin_software_array[$i]}
-                        doas rc-update add ${var_install_jellyfin_software_array[0]} boot 
-                    else
-                        $(f_get_distro_packager) add ${var_install_jellyfin_software_array[$i]}  
-                        rc-update add ${var_install_jellyfin_software_array[0]} boot 
-                    fi
-                done
-                f_update_software
+                $(f_get_distro_packager) install -y ${var_install_transmission_requirements[$i]}  
+            fi
+        done         
+    else
+       echo "- List of base software that will be installed using $(f_get_distro_packager):"
+        for i in "${!var_install_transmission_requirements[@]}"
+        do
+            echo " $i ${var_install_transmission_requirements[$i]}"
+        done
+        for i in "${!var_install_transmission_requirements[@]}"
+        do
+            echo " - and currently installing: $i ${var_install_transmission_requirements[$i]}"
+            if [[ "$EUID" -ne 0 ]]; then 
+                sudo $(f_get_distro_packager) install -y ${var_install_transmission_requirements[$i]}  
             else
-                echo "- but here is no version of Jellyfin for this architecture ($architecture);"
-            fi            
-        fi
+                $(f_get_distro_packager) install -y ${var_install_transmission_requirements[$i]}  
+            fi
+        done         
     fi
-  }
-# sudo mkdir /etc/apt/keyrings
-# DISTRO="$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release )"
-# CODENAME="$( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release )"
-# curl -fsSL https://repo.jellyfin.org/${DISTRO}/jellyfin_team.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/jellyfin.gpg
-# cat <<EOF | sudo tee /etc/apt/sources.list.d/jellyfin.sources
-# Types: deb
-# URIs: https://repo.jellyfin.org/${DISTRO}
-# Suites: ${CODENAME}
-# Components: main
-# Architectures: $( dpkg --print-architecture )
-# Signed-By: /etc/apt/keyrings/jellyfin.gpg
-# # EOF
-# sudo apt update
-# sudo apt install jellyfin -y
-# sudo setfacl -R -m u:busyneo:rx /media/ssdextern/
-f_install_jellyfin
+    f_update_software
+    f_config_transmission
+}
+f_install_transmission_requirements
