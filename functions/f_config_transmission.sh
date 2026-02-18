36f9c94e123d76a4c40dfa1e8bddebeff29d599d
Iulian Donici <iuliandonici@gmail.com>
2026-02-18 15:40:40 +0100

    Updated.

--- functions/f_config_transmission.sh
+++ functions/f_config_transmission.sh
@@ -3,28 +3,52 @@ function f_config_transmission() {
     # Verify if Transmission has been installed
     if [[ -f /usr/bin/transmission-daemon ]]; then
         echo "- Transmission is installed, now we'll config it:"
-        if [[ "$EUID" -ne 0 ]]; then 
-            sudo systemctl stop transmission-daemon.service
-            # sudo sed -i -e 's/Type=notify/Type=simple/g' /etc/systemd/system/transmission-daemon.service
-            sudo sed -i -e 's/Type=notify/Type=simple/g' /lib/systemd/system/transmission-daemon.service
-            sudo cp -r functions/f_config_transmission.json settings.json
-            sudo mv settings.json /var/lib/transmission-daemon/info/
-            sudo usermod -a -G debian-transmission $USER
-            sudo chmod 777 ~/
-            sudo chown debian-transmission:debian-transmission /var/lib/transmission-daemon/info/settings.json
-            sudo systemctl restart transmission-daemon.service
-            sudo systemctl daemon-reload
-        else
-            systemctl stop transmission-daemon.service
-            # sed -i -e 's/Type=notify/Type=simple/g' /etc/systemd/system/transmission-daemon.service
-            sed -i -e 's/Type=notify/Type=simple/g' /lib/systemd/system/transmission-daemon.service
-            cp -r functions/f_config_transmission.json settings.json 
-            mv settings.json /var/lib/transmission-daemon/info/
-            usermod -a -G debian-transmission $USER
-            chmod 777 ~/
-            chown debian-transmission:debian-transmission /var/lib/transmission-daemon/info/settings.json
-            systemctl restart transmission-daemon.service
-            systemctl daemon-reload
+        if [[ $(f_get_distro_packager) == "apk" ]]; then
+            if [[ "$EUID" -ne 0 ]]; then 
+                doas rc-service transmission-daemon stop
+                doas mv settings.json /var/lib/transmission/config/settings.json
+                doas addgroup $USER transmission
+                doas addgroup root transmission
+                doas mkdir /var/downloads
+                doas chown -R $USER:$USER /var/downloads
+                doas rc-update add transmission-daemon
+                doas lbu_commit device
+                doas rc-service transmission-daemon restart
+            else
+                rc-service transmission-daemon stop
+                mv settings.json /var/lib/transmission/config/settings.json
+                addgroup $USER transmission
+                addgroup root transmission
+                mkdir /var/downloads
+                chown -R $USER:$USER /var/downloads
+                rc-update add transmission-daemon
+                lbu_commit device
+                    rc-service transmission-daemon restart
+            fi
+        elif [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
+            if [[ "$EUID" -ne 0 ]]; then 
+                sudo systemctl stop transmission-daemon.service
+                # sudo sed -i -e 's/Type=notify/Type=simple/g' /etc/systemd/system/transmission-daemon.service
+                sudo sed -i -e 's/Type=notify/Type=simple/g' /lib/systemd/system/transmission-daemon.service
+                sudo cp -r functions/f_config_transmission.json settings.json
+                sudo mv settings.json /var/lib/transmission-daemon/info/
+                sudo usermod -a -G debian-transmission $USER
+                sudo chmod 777 ~/
+                sudo chown debian-transmission:debian-transmission /var/lib/transmission-daemon/info/settings.json
+                sudo systemctl restart transmission-daemon.service
+                sudo systemctl daemon-reload
+            else
+                systemctl stop transmission-daemon.service
+                # sed -i -e 's/Type=notify/Type=simple/g' /etc/systemd/system/transmission-daemon.service
+                sed -i -e 's/Type=notify/Type=simple/g' /lib/systemd/system/transmission-daemon.service
+                cp -r functions/f_config_transmission.json settings.json 
+                mv settings.json /var/lib/transmission-daemon/info/
+                usermod -a -G debian-transmission $USER
+                chmod 777 ~/
+                chown debian-transmission:debian-transmission /var/lib/transmission-daemon/info/settings.json
+                systemctl restart transmission-daemon.service
+                systemctl daemon-reload
+            fi
         fi
     else
         echo "- Transmission isn't installed."
