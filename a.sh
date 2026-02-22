c71c2e77958c5ed719f57f73ff68a4db77388781
Iulian Donici <iuliandonici@gmail.com>
2026-02-22 16:51:40 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -3,7 +3,7 @@ function f_config_kvm_images_ubuntu() {
     var_f_config_kvm_images_dir="/var/lib/libvirt/images"
     echo "- then downloading latest Ubuntu server ISO:"
     wget -q -X "*.10" http://cdimage.ubuntu.com/releases/ -O - | sed -e :a -e 's/<[^>]*>//g;/</N;//ba' | grep -E '^[[:space:][:space:]][1-9].*.04.*' | sed -e 's/\///g' -e 's/ //g' > ubuntu_last 
-    grep -oE '^[12468]*.[0-10][02468]*.*' ubuntu_last | sort -nr | head -1 > laststableubuntuversion
+    grep -oE '^[12468]*.[0-10][02468]*.*' ubuntu_last | sort -nr | head -2 > laststableubuntuversion
     var_latest_ubuntu_version=$(cat laststableubuntuversion)
     if ! [ -f $var_f_config_kvm_images_dir/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso ]; then
         wget https://releases.ubuntu.com/${var_latest_ubuntu_version}/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso
