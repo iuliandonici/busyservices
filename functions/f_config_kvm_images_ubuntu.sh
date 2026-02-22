343d67c44f637c15c8070eabb2b07fa88f6f7315
Iulian Donici <iuliandonici@gmail.com>
2026-02-22 18:42:55 +0100

    Updated.

--- functions/f_config_kvm_images_ubuntu.sh
+++ functions/f_config_kvm_images_ubuntu.sh
@@ -11,18 +11,23 @@ function f_config_kvm_images_ubuntu() {
             var_latest_ubuntu_version=$(tail -1 laststableubuntuversion)
             if ! [ -f $var_f_config_kvm_images_dir/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso ]; then
                 wget https://releases.ubuntu.com/${var_latest_ubuntu_version}/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso
+                wget https://releases.ubuntu.com/${var_latest_ubuntu_version}/ubuntu-$var_latest_ubuntu_version-desktop-amd64.iso
                 if [[ "$EUID" -ne 0 ]]; then 
                     if [[ $(f_get_distro_packager) == "apk" ]]; then
                         doas rsync -aP --remove-source-files ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso $var_f_config_kvm_images_dir
+                        doas rsync -aP --remove-source-files ubuntu-$var_latest_ubuntu_version-desktop-amd64.iso $var_f_config_kvm_images_dir
                     else
                         sudo rsync -aP --remove-source-files ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso $var_f_config_kvm_images_dir
+                        sudo rsync -aP --remove-source-files ubuntu-$var_latest_ubuntu_version-desktop-amd64.iso $var_f_config_kvm_images_dir
                     fi
                 else
                     rsync -aP --remove-source-files ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso $var_f_config_kvm_images_dir
+                    rsync -aP --remove-source-files ubuntu-$var_latest_ubuntu_version-desktop.iso $var_f_config_kvm_images_dir
                 fi
             else
                 echo "- but latest Ubuntu LTS version is already present at: "
                 echo $var_f_config_kvm_images_dir/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso
+                echo $var_f_config_kvm_images_dir/ubuntu-$var_latest_ubuntu_version-desktop-amd64.iso
             fi
         fi
     else
