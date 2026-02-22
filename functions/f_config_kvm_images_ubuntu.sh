4b5754cf6242f54054213530940e61a952b2d381
Iulian Donici <iuliandonici@gmail.com>
2026-02-22 17:17:46 +0100

    Updated.

--- functions/f_config_kvm_images_ubuntu.sh
+++ functions/f_config_kvm_images_ubuntu.sh
@@ -3,10 +3,14 @@ function f_config_kvm_images_ubuntu() {
     var_f_config_kvm_images_dir="/var/lib/libvirt/images"
     echo "- then downloading latest Ubuntu server ISO:"
     wget -q -X "*.10" http://cdimage.ubuntu.com/releases/ -O - | sed -e :a -e 's/<[^>]*>//g;/</N;//ba' | grep -E '^[[:space:][:space:]][1-9].*.04.*' | sed -e 's/\///g' -e 's/ //g' > ubuntu_last 
-    grep -oE '^[12468]*.[0-10][02468]*.*' ubuntu_last | sort -nr | head -1 > laststableubuntuversion
-    var_latest_ubuntu_version=$(cat laststableubuntuversion)
+    grep -oE '^[12468]*.[0-10][02468]*.*' ubuntu_last | sort -nr | head -2 > laststableubuntuversion
+    var_latest_ubuntu_version=$(cat laststableubuntuversion | head -1)
     if ! [ -f $var_f_config_kvm_images_dir/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso ]; then
-        wget https://releases.ubuntu.com/${var_latest_ubuntu_version}/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso
+        start_wget=$(wget https://releases.ubuntu.com/${var_latest_ubuntu_version}/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso)
+        if [[ "$start_wget" -eq 0 ]]; then
+            var_latest_ubuntu_version=$(tail -1 laststableubuntuversion)
+            wget https://releases.ubuntu.com/${var_latest_ubuntu_version}/ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso        
+        fi
         if [[ "$EUID" -ne 0 ]]; then 
             sudo rsync -aP --remove-source-files ubuntu-$var_latest_ubuntu_version-live-server-amd64.iso $var_f_config_kvm_images_dir
         else
