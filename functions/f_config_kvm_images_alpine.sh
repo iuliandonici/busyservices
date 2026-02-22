e6802b17a7248f9e6e36179969db63da26dcea60
Iulian Donici <iuliandonici@gmail.com>
2026-02-22 18:28:40 +0100

    Updated.

--- functions/f_config_kvm_images_alpine.sh
+++ functions/f_config_kvm_images_alpine.sh
@@ -4,14 +4,14 @@ function f_config_kvm_images_alpine() {
     var_f_config_kvm_images_arch_alpine=("x86" "x86_64" "aarch64")
     var_f_config_kvm_images_repo_alpine="https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases"
     var_f_config_kvm_images_repo_alpine_dir="dl-cdn.alpinelinux.org/alpine/latest-stable/releases"
-    echo "- Downloading Alpine ISO for the following architectures:"
+    echo " - downloading Alpine ISO for the following architectures:"
     for i in "${!var_f_config_kvm_images_arch_alpine[@]}"
     do
         echo " $i ${var_f_config_kvm_images_arch_alpine[$i]}"
     done
     for i in "${!var_f_config_kvm_images_arch_alpine[@]}"
     do
-    echo "- and currently downloading Alpine ISOs for:"
+    echo " - and currently downloading Alpine ISOs for:"
         echo " $i ${var_f_config_kvm_images_arch_alpine[$i]}"
         # Getting a list of ISOs, filtering them and putting them into a file
         wget --no-parent $var_f_config_kvm_images_repo_alpine/${var_f_config_kvm_images_arch_alpine[$i]} -O - | grep -E "alpine-standard" | grep -Ev '_rc|asc|sha256|sha512' | sed -e 's/<[^>]*>//g' | awk '{print $1}' | sort -nr | head -1 > alpineversions
