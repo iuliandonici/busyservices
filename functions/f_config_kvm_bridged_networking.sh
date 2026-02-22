5c96b861ccb1f63a780fe1ace1beb2c1573bc562
Iulian Donici <iuliandonici@gmail.com>
2026-02-22 18:25:11 +0100

    Updated.

--- functions/f_config_kvm_bridged_networking.sh
+++ functions/f_config_kvm_bridged_networking.sh
@@ -3,14 +3,14 @@ function f_config_kvm_bridged_networking() {
     echo "- Creating a bridged network interface so we can get the same subnet IPs as the KVM host;"
     if [[ $(f_get_distro_packager) == "apk" ]]; then
         if [[ "$EUID" -ne 0 ]]; then 
-            sudo cp -r functions/f_config_kvm_netfilter_bridge /etc/sysctl.d/99-kvm-netfilter-bridge.conf
-            sudo ip link add bridge0 type bridge
+            doas cp -r functions/f_config_kvm_netfilter_bridge /etc/sysctl.d/99-kvm-netfilter-bridge.conf
+            doas ip link add bridge0 type bridge
             # sudo ip link set ens33 master bridge0
-            sudo ip address add dev bridge0 192.168.50.0/24
-            sudo modprobe br_netfilter
-            sudo echo "br_netfilter" >> br_netfilter.conf
-            sudo mv br_netfilter.conf /etc/modules-load.d/br_netfilter.conf
-            sudo sysctl -p /etc/sysctl.d/99-kvm-netfilter-bridge.conf
+            doas ip address add dev bridge0 192.168.50.0/24
+            doas modprobe br_netfilter
+            doas echo "br_netfilter" >> br_netfilter.conf
+            doas mv br_netfilter.conf /etc/modules-load.d/br_netfilter.conf
+            doas sysctl -p /etc/sysctl.d/99-kvm-netfilter-bridge.conf
         else
             cp -r functions/f_config_kvm_netfilter_bridge /etc/sysctl.d/99-kvm-netfilter-bridge.conf
             ip link add bridge0 type bridge
