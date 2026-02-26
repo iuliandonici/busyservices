57714313223dd9cf94374d6220893edd40c1f047
Iulian Donici <iuliandonici@gmail.com>
2026-02-26 17:38:53 +0100

    Transitioning from simple sudo towards a more dynamic security tool check.

--- functions/f_add_repo_brave_browser.sh
+++ functions/f_add_repo_brave_browser.sh
@@ -3,40 +3,36 @@ function f_add_repo_brave_browser() {
     source functions/f_update_software.sh
     source functions/f_add_repo_flathub.sh
     echo " - and currently adding the Brave browser repo using $(f_get_distro_packager):"
-    if [[ $(f_get_distro_packager) == "apk" ]]; then
-        if [[ $(f_check_networks) == "UP" ]]; then
-            echo "- and since Brave browser isn't available natively on $(f_get_distro_id) using the $(f_get_distro_packager) packager so instead we're using flatpak;"
+    if [[ $(f_check_networks) == "UP" ]]; then
+        echo "- and since Brave browser isn't available natively on $(f_get_distro_id) using the $(f_get_distro_packager) packager so instead we're using flatpak;"
+        if [[ $(f_get_distro_packager) == "apk" ]]; then
             f_add_repo_flathub
             flatpak install -y flathub com.brave.Browser
             f_update_software
-        else
-            echo "- but we can't install Brave browser because the networks are down;"
-        fi
-    elif [[ "$(f_get_distro_packager)" == "dnf" ]]; then
-         if [[ "$EUID" -ne 0 ]]; then        
-            sudo $(f_get_distro_packager) install -y dnf-plugins-core
-            sudo $(f_get_distro_packager) config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-            sudo curl -fsSLO "https://dl.brave.com/install.sh{,.asc}" && gpg --keyserver hkps://keys.openpgp.org --recv-keys D16166072CACDF2C9429CBF11BF41E37D039F691 && gpg --verify install.sh.asc
-            $(f_get_distro_packager) config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-            sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-        else
-            $(f_get_distro_packager) install -y dnf-plugins-core
-            curl -fsSLO "https://dl.brave.com/install.sh{,.asc}" && gpg --keyserver hkps://keys.openpgp.org --recv-keys D16166072CACDF2C9429CBF11BF41E37D039F691 && gpg --verify install.sh.asc
-            $(f_get_distro_packager) config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-            rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-        fi
-    elif [[ "$(f_get_distro_packager)" == "zypper" ]]; then
-        if [[ "$EUID" -ne 0 ]]; then 
-            sudo $(f_get_distro_packager) addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-            sudo $(f_get_distro_packager) rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-        else
-            $(f_get_distro_packager) addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-            rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-        fi
-    elif [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
-        if [[ $(f_check_networks) == "UP" ]]; then
+        elif [[ "$(f_get_distro_packager)" == "dnf" ]]; then
+            if [[ "$EUID" -ne 0 ]]; then        
+                $(f_get_security_utility) $(f_get_distro_packager) install -y dnf-plugins-core
+                $(f_get_security_utility) $(f_get_distro_packager) config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+                $(f_get_security_utility) curl -fsSLO "https://dl.brave.com/install.sh{,.asc}" && gpg --keyserver hkps://keys.openpgp.org --recv-keys D16166072CACDF2C9429CBF11BF41E37D039F691 && gpg --verify install.sh.asc
+                $(f_get_distro_packager) config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+                $(f_get_security_utility) rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+            else
+                $(f_get_distro_packager) install -y dnf-plugins-core
+                curl -fsSLO "https://dl.brave.com/install.sh{,.asc}" && gpg --keyserver hkps://keys.openpgp.org --recv-keys D16166072CACDF2C9429CBF11BF41E37D039F691 && gpg --verify install.sh.asc
+                $(f_get_distro_packager) config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+                rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+            fi
+        elif [[ "$(f_get_distro_packager)" == "zypper" ]]; then
             if [[ "$EUID" -ne 0 ]]; then 
-                sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+                $(f_get_security_utility) $(f_get_distro_packager) addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+                $(f_get_security_utility) $(f_get_distro_packager) rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+            else
+                $(f_get_distro_packager) addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+                rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+            fi
+        elif [[ "$(f_get_distro_packager)" == "apt" || "$(f_get_distro_packager)" == "apt-get" ]]; then
+            if [[ "$EUID" -ne 0 ]]; then 
+                $(f_get_security_utility) curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
                 # Setting a variable for getting the machine's architecture
                 architecture=$(uname -m)
                 if [[ $architecture=="x64" || $architecture=="x86_64" ]]; then
@@ -54,8 +50,8 @@ function f_add_repo_brave_browser() {
                     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|tee /etc/apt/sources.list.d/brave-browser-release.list
                 fi
             fi
-        else
-            echo "- but we can't install Brave browser because the networks are down;"
         fi
+    else
+        echo "- but we can't install Brave browser because the networks are down;"
     fi
 }
--- functions/f_add_repo_flathub.sh
+++ functions/f_add_repo_flathub.sh
@@ -1,48 +1,44 @@
 #!/bin/bash
 function f_add_repo_flathub() {
   # Installing using the official documentation
-  source functions/f_check_networks.sh
-  source functions/f_get_distro_id.sh
+  # source functions/f_check_networks.sh
+  # source functions/f_get_distro_id.sh
   # A variable to see what environment is currently installed
   var_f_add_repo_flathub_desktop=$(printf 'desktop: %s' "$XDG_CURRENT_DESKTOP")
   if [[ $(f_get_distro_packager) == "apk" ]]; then
-    if [[ $(f_check_networks) == "UP" ]]; then
-      f_update_software
-      if [[ "$EUID" -ne 0 ]]; then
-        # Install the package
-        doas apk add flatpak
-        # Install the Software Flatpak plugin for KDE
-        echo "- and we are using $var_f_add_repo_flathub_desktop;"
-        echo "- currently installing the Software Flatpak plugin;"
+    f_update_software
+    if [[ "$EUID" -ne 0 ]]; then
+      # Install the package
+      $(f_get_security_utility) apk add flatpak
+      # Install the Software Flatpak plugin for KDE
+      echo "- and we are using $var_f_add_repo_flathub_desktop;"
+      echo "- currently installing the Software Flatpak plugin;"
+      # if its' KDE
+      if [[ $var_f_add_repo_flathub_desktop == "KDE" ]]; then
+        $(f_get_security_utility) apk add discover-backend-flatpak
+      # or Gnome
+      elif [[ $var_f_add_repo_flathub_desktop == "Gnome" ]]; then
+        $(f_get_security_utility) apk add gnome-software-plugin-flatpak
+      fi
+      # Add the Flathub repository
+      echo "- currently adding the Flathub repository;"
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    else
+      # Install the package
+      apk add flatpak
+      # Install the Software Flatpak plugin for KDE
+      echo "- and we're using $var_f_add_repo_flathub_desktop;"
+      echo "- currently installing the Software Flatpak plugin;"
         # if its' KDE
         if [[ $var_f_add_repo_flathub_desktop == "KDE" ]]; then
-          doas apk add discover-backend-flatpak
+          apk add discover-backend-flatpak
         # or Gnome
         elif [[ $var_f_add_repo_flathub_desktop == "Gnome" ]]; then
-          doas apk add gnome-software-plugin-flatpak
+          apk add gnome-software-plugin-flatpak
         fi
-        # Add the Flathub repository
-        echo "- currently adding the Flathub repository;"
-        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-      else
-        # Install the package
-        apk add flatpak
-        # Install the Software Flatpak plugin for KDE
-        echo "- and we're using $var_f_add_repo_flathub_desktop;"
-        echo "- currently installing the Software Flatpak plugin;"
-          # if its' KDE
-          if [[ $var_f_add_repo_flathub_desktop == "KDE" ]]; then
-            apk add discover-backend-flatpak
-          # or Gnome
-          elif [[ $var_f_add_repo_flathub_desktop == "Gnome" ]]; then
-            apk add gnome-software-plugin-flatpak
-          fi
-        # Add the Flathub repository
-        echo "- currently adding the Flathub repository;"
-        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-      fi
-    else
-      echo "- but we can't install Flatpak because the networks are down;"
+      # Add the Flathub repository
+      echo "- currently adding the Flathub repository;"
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
     fi
   fi
 }
