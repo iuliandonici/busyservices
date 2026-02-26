11bea1248be5b90e30f00975606145f78bdcb76f
Iulian Donici <iuliandonici@gmail.com>
2026-02-26 19:14:33 +0100

    Transitioning from simple sudo towards a more dynamic security tool check.

--- functions/f_config_kde_networking.sh
+++ functions/f_config_kde_networking.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 function f_config_kde_networking() {
+    source functions/f_get_security_utility.sh
     echo "- and currently configuring KDE networking;"
     if [[ $(f_get_distro_packager) == "apk" ]]; then
         if [[ -f /etc/NetworkManager/NetworkManager.conf ]]; then
             echo "- but the NetworkManager.conf file already exists, so we're not going to configure the networking;"
         else
             if [[ "$EUID" -ne 0 ]]; then
-                doas adduser $USER plugdev
+                $(f_get_security_utility) adduser $USER plugdev
                 echo "[main]
             dhcp=internal
             plugins=ifupdown,keyfile
@@ -18,17 +19,17 @@ function f_config_kde_networking() {
             wifi.scan-rand-mac-address=yes
             wifi.backend=wpa_supplicant
             wifi.wpa_supplicant.autoconnect=yes" >> NetworkManager.conf
-                doas mv NetworkManager.conf /etc/NetworkManager/
-                doas mkdir -p /etc/NetworkManager/conf.d/
+                $(f_get_security_utility) mv NetworkManager.conf /etc/NetworkManager/
+                $(f_get_security_utility) mkdir -p /etc/NetworkManager/conf.d/
                 echo "[main]
             auth-polkit=false" >> any-user.conf
-                doas mv any-user.conf /etc/NetworkManager/conf.d/
-                doas rc-service networking stop
-                doas rc-service wpa_supplicant stop
-                doas rc-service networkmanager restart
-                doas rc-update add networkmanager default
-                doas rc-update del networking boot
-                doas rc-update del wpa_supplicant boot
+                $(f_get_security_utility) mv any-user.conf /etc/NetworkManager/conf.d/
+                $(f_get_security_utility) rc-service networking stop
+                $(f_get_security_utility) rc-service wpa_supplicant stop
+                $(f_get_security_utility) rc-service networkmanager restart
+                $(f_get_security_utility) rc-update add networkmanager default
+                $(f_get_security_utility) rc-update del networking boot
+                $(f_get_security_utility) rc-update del wpa_supplicant boot
             else
                 adduser $USER plugdev
                 echo "[main] 
