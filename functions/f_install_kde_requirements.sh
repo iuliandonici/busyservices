08c0cd3f28214d338654f934fb062b129763ba88
Iulian Donici <iuliandonici@gmail.com>
2026-02-26 19:22:03 +0100

    Transitioning from simple sudo towards a more dynamic security tool check.

--- functions/f_install_kde_requirements.sh
+++ functions/f_install_kde_requirements.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 var_install_kde_software_array=("plasma elogind" "polkit-elogind" "kde-applications-admin" "kde-applications-network" "kde-applications-utils" "kde-applications-base" "font-terminus" "font-inconsolata" "font-dejavu" "font-noto" "font-noto-cjk" "font-awesome" "font-noto-extra" "font-liberation" "python3" "networkmanager" "networkmanager-tui" "networkmanager-cli" "plasma-nm" "network-manager-applet" "networkmanager-wifi" "virt-manager" "vlc" "gvfs-fuse" "udisks2" "device-mapper" "lvm2" "isoimagewriter")
+var_install_kde_software_array_alpine=("plasma elogind" "polkit-elogind" "kde-applications-admin" "kde-applications-network" "kde-applications-utils" "kde-applications-base" "font-terminus" "font-inconsolata" "font-dejavu" "font-noto" "font-noto-cjk" "font-awesome" "font-noto-extra" "font-liberation" "python3" "networkmanager" "networkmanager-tui" "networkmanager-cli" "plasma-nm" "network-manager-applet" "networkmanager-wifi" "virt-manager" "vlc" "gvfs-fuse" "udisks2" "device-mapper" "lvm2" "isoimagewriter")
 function f_install_kde_requirements() {
     source functions/f_update_software.sh
     source functions/f_get_distro_packager.sh
     source functions/f_config_kde.sh
     source functions/f_config_kde_networking.sh
     echo "- Installing KDE desktop environment:"
-    echo "- and here's a list of base software that will be installed using $(f_get_distro_packager):"
-    for i in "${!var_install_base_software_array[@]}"
-    do
-        echo " $i ${var_install_kde_software_array[$i]}"
-    done
     if [[ $(f_get_distro_packager) == "apk" ]]; then
         if [[ $(f_check_networks) == "UP" ]]; then
-            f_update_software
-            for i in "${!var_install_kde_software_array[@]}"
+            echo "- and here's a list of base software that will be installed using $(f_get_distro_packager):"
+            for i in "${!var_install_kde_software_array_alpine[@]}"
             do
-                echo "- and currently installing: $i ${var_install_kde_software_array[$i]}"
+                echo " $i ${var_install_kde_software_array_alpine[$i]}"
+            done
+            f_update_software
+            for i in "${!var_install_kde_software_array_alpine[@]}"
+            do
+                echo "- and currently installing: $i ${var_install_kde_software_array_alpine[$i]}"
                 if [[ "$EUID" -ne 0 ]]; then 
                     $(f_get_security_utility) $(f_get_distro_packager) add ${var_install_kde_software_array[$i]}  
                 else
