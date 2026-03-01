64991c41c8cb05ab7210e82d53f97ab9afc659a4
Iulian Donici <iuliandonici@gmail.com>
2026-03-01 17:03:52 +0100

    Updated.

--- functions/f_config_xfce.sh
+++ functions/f_config_xfce.sh
@@ -6,12 +6,12 @@ function f_config_xfce() {
     if [[ $(f_check_networks) == "UP" ]]; then
         if [[ $(f_get_distro_packager) == "apk" ]]; then
             if [[ "$EUID" -ne 0 ]]; then
+                $(f_get_security_utility) adduser $USER wheel
                 $(f_get_security_utility) setup-xorg-base
                 $(f_get_security_utility) apk add lightdm dbus
                 $(f_get_security_utility) rc-update add dbus
                 $(f_get_security_utility) rc-update add lightdm
                 $(f_get_security_utility) rc-service lightdm restart
-                $(f_get_security_utility) adduser $USER wheel
 #                 $(f_get_security_utility) echo -e "#!/bin/bash
 # doas startx" > initx.start
 #                 $(f_get_security_utility) mv initx.start /etc/local.d/
@@ -19,17 +19,12 @@ function f_config_xfce() {
 #                 $(f_get_security_utility) chmod +x /etc/local.d/initx.start
                 # sudo f_install_busychrome_audio
             else
+                adduser $USER wheel
                 setup-xorg-base
-                apk add lightdm
+                apk add lightdm dbus
+                rc-update add dbus
                 rc-update add lightdm
-                rc-update add local
-                rc-service lightdm restart                
-                echo -e "#!/bin/bash
-startx" > initx.start
-                mv initx.start /etc/local.d/
-                # chown -R $USER:$USER /etc/local.d/initx.start
-                chmod +x /etc/local.d/initx.start
-                f_install_busychrome_audio
+                rc-service lightdm restart
             fi
         elif [[ $(f_get_distro_packager) == "dnf" || $(f_get_distro_packager) == "zypper" ]]; then
             echo "- No tests done for dns/zypper;"
