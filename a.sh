98d5b8c1b1f3d53b3905bcae407a6ca5fa7020ce
Iulian Donici <iuliandonici@gmail.com>
2026-03-05 16:12:00 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 function f_install_cosmic() {
   echo " - Installing Cosmic desktop;"
+  doas setup-xorg-base
   echo "- and currently adding the current user to the wheel group;"
   doas adduser $USER wheel
   echo "- and currently installing requirements for the environment;"
-  doas apk add elogind dbus polkit-elogind cosmic-session cosmic-term cosmic-edit neofetch
+  doas apk add elogind dbus polkit-elogind
   doas rc-update add elogind
   doas rc-update add dbus
   doas rc-update add polkit
