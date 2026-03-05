0d53b1d87fa1a9f76847c02b773efbf870e5a01b
Iulian Donici <iuliandonici@gmail.com>
2026-03-05 14:49:29 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-source functions/f_update_software.sh
-source functions/f_install_xfce.sh
-f_install_xfce
+function f_install_cosmic() {
+  echo " - Installing Cosmic desktop;"
+  echo "- and currently adding the current user to the wheel group;"
+  doas adduser $USER wheel
+  doas apk add elogind dbus polkit-elogind cosmic-session cosmic-term cosmic-edit
+  doas rc-update add elogind
+  doas rc-update add dbus
+  doas rc-update add polkit
+  doas setup-devd udev
+}
+f_install_cosmic
