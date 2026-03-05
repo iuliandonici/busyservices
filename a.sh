90145400b313c88aba78ba4bd0e307ad1022070e
Iulian Donici <iuliandonici@gmail.com>
2026-03-05 16:40:25 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 function f_install_cosmic() {
   echo " - Installing Cosmic desktop:"
+  echo "- and currently adding the current user to the wheel group;"
+  doas adduser $USER wheel
+  echo "- and currently setting up udev;" 
   doas setup-devd udev
   echo "- and currently installing xorg-base;"
   doas setup-xorg-base
-  echo "- and currently adding the current user to the wheel group;"
-  doas adduser $USER wheel
   echo "- and currently installing requirements for the environment;"
   doas apk add mate-desktop-environment lxdm adwaita-icon-theme faenza-icon-theme font-dejavu
   gvfs_pkgs=$(apk search gvfs -q | grep -v '\-dev' | grep -v '\-lang' | grep -v '\-doc')
