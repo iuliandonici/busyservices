5e4270e211521df6caddc1b78289a2ebcff677aa
Iulian Donici <iuliandonici@gmail.com>
2026-03-06 16:59:36 +0100

    Getting rid of network checking before installing.

--- a.sh
+++ a.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-function f_install_mate() {
-      source functions/f_install_busychrome_audio.sh
-      source functions/f_install_lite_xl.sh
-      source functions/f_install_bluetooth.sh
-      source functions/f_install_brave_browser.sh
-      # f_install_kde_requirements
-
-  doas setup-desktop mate
-      f_install_busychrome_audio
-      f_install_bluetooth
-      f_install_lite_xl
-      f_install_brave_browser
-
+function f_install_kde_themes() {
+      
 }
-f_install_mate
+f_install_kde_themes
