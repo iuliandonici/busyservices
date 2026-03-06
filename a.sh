d7868171ef5f0d6e1fb97b1f6ffe0daa213de9ba
Iulian Donici <iuliandonici@gmail.com>
2026-03-07 00:08:34 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 function f_install_kde_themes() {
- cp -r busykdeplasmatheme-sweet/ ~/.local/share/plasma/desktopthemes/
- cp -r busykdeplasmaplugin-sweet/ ~/.local/share/plasma/look-and-feel/
+ mkdir -p ~/.local/share/plasma/desktoptheme/
+ mkdir -p ~/.local/share/plasma/look-and-feel/
+ cp -r busykdethemes/Sweet-Ambar-Blue/ ~/.local/share/plasma/desktoptheme/
+ cp -r busykdethemes/busykdeplasmaplugin-sweet/ ~/.local/share/plasma/look-and-feel/
 }
 f_install_kde_themes
