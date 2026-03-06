afb9ab555e51c43cc291d26d04c68b95ef869abe
Iulian Donici <iuliandonici@gmail.com>
2026-03-06 23:43:14 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 function f_install_kde_themes() {
+ cp -r busykdeplasmatheme-sweet/ ~/.local/share/plasma/desktopthemes/
  cp -r busykdeplasmaplugin-sweet/ ~/.local/share/plasma/look-and-feel/
 }
 f_install_kde_themes
