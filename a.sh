effbd3ba27bf37d8247b10293e1dd871add89d76
Iulian Donici <iuliandonici@gmail.com>
2026-03-07 18:49:16 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -2,6 +2,7 @@
 function f_install_kde_themes() {
   doas mkdir -p ~/.local/share/plasma/desktoptheme/
   doas mkdir -p ~/.local/share/plasma/look-and-feel/
+  cd busytheme/
   doas  cp -r Sweet-Ambar-Blue/ ~/.local/share/plasma/desktoptheme/
   doas  cp -r Sweet-Ambar-Blue/ ~/.local/share/plasma/look-and-feel/
   doas  cp -r Sweet-Ambar-Blue/ /usr/share/plasma/desktoptheme/
@@ -10,6 +11,6 @@ function f_install_kde_themes() {
   doas cp -r candy-icons/ /usr/share/icons/
   doas cp -r Sweet-cursors/ /usr/share/icons/
   doas cp -r Sweet-Wallpapers/ /usr/share/wallpapers/
-  doas cp -r sddm/Sweet/ /usr/share/sddm/themes/
+  doas cp -r Sweet/ /usr/share/sddm/themes/
 }
 f_install_kde_themes
