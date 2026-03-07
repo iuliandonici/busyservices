1c80b338fa34839d9cc1df413d7a4fb0da313071
Iulian Donici <iuliandonici@gmail.com>
2026-03-07 19:42:44 +0100

    Updated.

--- a.sh
+++ a.sh
@@ -1,3 +1,3 @@
-#!bin/bash
+#!/bin/bash
 source functions/f_install_kde_themes.sh
 f_install_kde_themes
--- functions/f_install_kde_requirements.sh
+++ functions/f_install_kde_requirements.sh
@@ -6,6 +6,7 @@ function f_install_kde_requirements() {
     source functions/f_get_distro_packager.sh
     source functions/f_config_kde.sh
     source functions/f_config_kde_networking.sh
+    source functions/f_install_kde_themes.sh
     echo " - Installing KDE desktop environment:"
 #    if [[ $(f_check_networks) == "UP" ]]; then
         if [[ $(f_get_distro_packager) == "apk" ]]; then
@@ -25,6 +26,7 @@ function f_install_kde_requirements() {
                 fi
             done
         f_config_kde
+        f_install_kde_themes
         elif [[ $(f_get_distro_packager) == "dnf" || $(f_get_distro_packager) == "zypper" ]]; then
             echo "- and here's a list of base software that will be installed using $(f_get_distro_packager):"
             for i in "${!var_install_kde_software_array[@]}"
