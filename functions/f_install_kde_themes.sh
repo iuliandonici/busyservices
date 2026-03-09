30a3ba0d2cab48652dd7e0a028d572ce90efbb2b
Iulian Donici <iuliandonici@gmail.com>
2026-03-09 19:44:03 +0100

    Updated.

--- functions/f_install_kde_themes.sh
+++ functions/f_install_kde_themes.sh
@@ -10,16 +10,16 @@ if [[ $(f_check_networks) == "UP" ]]; then
     mkdir -p ~/.local/share/color-schemes/
     rm -rf busykdethemes/
     git clone git@github.com:iuliandonici/busykdethemes.git
-    #  $(f_get_security_utility) cp -r Sweet-Ambar-Blue/ ~/.local/share/plasma/desktoptheme/
-    #  $(f_get_security_utility) cp -r Sweet-Ambar-Blue/ ~/.local/share/plasma/look-and-feel/
-    cp -r busykdethemes/busytheme/Sweet-Ambar-Blue/ ~/.local/share/plasma/desktoptheme/
-    cp -r busykdethemes/busytheme/Sweet-Ambar-Blue/ ~/.local/share/plasma/look-and-feel/ 
+    #  $(f_get_security_utility) cp -r busytheme-plasma/ ~/.local/share/plasma/desktoptheme/
+    #  $(f_get_security_utility) cp -r busytheme-plasma/ ~/.local/share/plasma/look-and-feel/
+    cp -r busykdethemes/busytheme/busytheme-plasma/ ~/.local/share/plasma/desktoptheme/
+    cp -r busykdethemes/busytheme/busytheme-plasma/ ~/.local/share/plasma/look-and-feel/ 
     cp -r busykdethemes/busytheme/busykdeplugin-sweet/ ~/.local/share/plasma/look-and-feel/
-    cp -r busykdethemes/busytheme/SweetAmbarBlue.colors ~/.local/share/color-schemes/SweetAmbarBlue.colors
-    $(f_get_security_utility) cp -r busykdethemes/busytheme/candy-icons/ /usr/share/icons/
-    $(f_get_security_utility) cp -r busykdethemes/busytheme/Sweet-cursors/ /usr/share/icons/
-    $(f_get_security_utility) cp -r busykdethemes/busytheme/Sweet-Wallpapers/ /usr/share/wallpapers/
-    $(f_get_security_utility) cp -r busykdethemes/busytheme/Sweet-Ambar-Blue-Plasma-6/ /usr/share/sddm/themes/
+    cp -r busykdethemes/busytheme/busytheme.colors ~/.local/share/color-schemes/busytheme.colors
+    $(f_get_security_utility) cp -r busykdethemes/busytheme/busytheme-icons/ /usr/share/icons/
+    $(f_get_security_utility) cp -r busykdethemes/busytheme/busytheme-cursors/ /usr/share/icons/
+    $(f_get_security_utility) cp -r busykdethemes/busytheme/busytheme-wallpapers/ /usr/share/wallpapers/
+    $(f_get_security_utility) cp -r busykdethemes/busytheme/busytheme-sddm/ /usr/share/sddm/themes/
     rm -rf busykdethemes/
   else
     mkdir -p ~/.local/share/plasma/desktoptheme/
@@ -27,15 +27,15 @@ if [[ $(f_check_networks) == "UP" ]]; then
     mkdir -p ~/.local/share/color-schemes/
     rm -rf busykdethemes/
     git clone git@github.com:iuliandonici/busykdethemes.git
-    #  cp -r Sweet-Ambar-Blue/ ~/.local/share/plasma/desktoptheme/
-    #  cp -r Sweet-Ambar-Blue/ ~/.local/share/plasma/look-and-feel/
-    cp -r busykdethemes/busytheme/Sweet-Ambar-Blue/ /usr/share/plasma/desktoptheme/
-    cp -r busykdethemes/busytheme/Sweet-Ambar-Blue/ /usr/share/plasma/look-and-feel/ 
-    cp -r busykdethemes/busytheme/SweetAmbarBlue.colors ~/.local/share/color-schemes/SweetAmbarBlue.colors
-    cp -r busykdethemes/busytheme/candy-icons/ /usr/share/icons/
-    cp -r busykdethemes/busytheme/Sweet-cursors/ /usr/share/icons/
-    cp -r busykdethemes/busytheme/Sweet-Wallpapers/ /usr/share/wallpapers/
-    cp -r busykdethemes/busytheme/Sweet-Ambar-Blue-Plasma-6/ /usr/share/sddm/themes/
+    #  cp -r busytheme-plasma/ ~/.local/share/plasma/desktoptheme/
+    #  cp -r busytheme-plasma/ ~/.local/share/plasma/look-and-feel/
+    cp -r busykdethemes/busytheme/busytheme-plasma/ /usr/share/plasma/desktoptheme/
+    cp -r busykdethemes/busytheme/busytheme-plasma/ /usr/share/plasma/look-and-feel/ 
+    cp -r busykdethemes/busytheme/busytheme.colors ~/.local/share/color-schemes/busytheme.colors
+    cp -r busykdethemes/busytheme/busytheme-icons/ /usr/share/icons/
+    cp -r busykdethemes/busytheme/busytheme-cursors/ /usr/share/icons/
+    cp -r busykdethemes/busytheme/busytheme-wallpapers/ /usr/share/wallpapers/
+    cp -r busykdethemes/busytheme/busytheme-sddm/ /usr/share/sddm/themes/
     rm -rf busykdethemes/
   fi
 else
