6509d0202f9d72f774fb4d406e477b03e6420392
Iulian Donici <iuliandonici@gmail.com>
2026-03-09 18:27:47 +0100

    Updated.

--- functions/f_install_kde_themes.sh
+++ functions/f_install_kde_themes.sh
@@ -3,16 +3,17 @@ function f_install_kde_themes() {
   source functions/f_update_software.sh
 echo " - and currently installing KDE themes:"
 if [[ $(f_check_networks) == "UP" ]]; then
-  if [[ "$EUID" -ne 0 ]]; then 
-    $(f_get_security_utility) mkdir -p ~/.local/share/plasma/desktoptheme/
-    $(f_get_security_utility) mkdir -p ~/.local/share/plasma/look-and-feel/
-    $(f_get_security_utility) mkdir -p ~/.local/share/color-schemes/
+  if [[ "$EUID" -ne 0 ]]; then
+    $(f_get_security_utility) rm -rf ~/.local/share/plasma/ ~/.local/share/plasma/color-schemes
+    mkdir -p ~/.local/share/plasma/desktoptheme/
+    mkdir -p ~/.local/share/plasma/look-and-feel/
+    mkdir -p ~/.local/share/color-schemes/
     rm -rf busykdethemes/
     git clone git@github.com:iuliandonici/busykdethemes.git
     #  $(f_get_security_utility) cp -r Sweet-Ambar-Blue/ ~/.local/share/plasma/desktoptheme/
     #  $(f_get_security_utility) cp -r Sweet-Ambar-Blue/ ~/.local/share/plasma/look-and-feel/
-    $(f_get_security_utility) cp -r busykdethemes/busytheme/Sweet-Ambar-Blue/ /usr/share/plasma/desktoptheme/
-    $(f_get_security_utility) cp -r busykdethemes/busytheme/Sweet-Ambar-Blue/ /usr/share/plasma/look-and-feel/ 
+    $(f_get_security_utility) cp -r busykdethemes/busytheme/Sweet-Ambar-Blue/ ~/.local/share/plasma/desktoptheme/
+    $(f_get_security_utility) cp -r busykdethemes/busytheme/Sweet-Ambar-Blue/ ~/.local/share/plasma/look-and-feel/ 
     $(f_get_security_utility) cp -r busykdethemes/busytheme/SweetAmbarBlue.colors ~/.local/share/color-schemes/SweetAmbarBlue.colors
     $(f_get_security_utility) cp -r busykdethemes/busytheme/candy-icons/ /usr/share/icons/
     $(f_get_security_utility) cp -r busykdethemes/busytheme/Sweet-cursors/ /usr/share/icons/
